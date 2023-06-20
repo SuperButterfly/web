3715feb897f5dbfa5060c8a6e642f8c76c3fb23b
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-06-20 14:49:10 -0500

    ejercicio  #10020

--- src/Components/workspace/menu/menu.js
+++ src/Components/workspace/menu/menu.js
@@ -176,7 +176,6 @@ const Menu = ({ filteredWorkspaces }) => {
                   id={idx === 0 ? "first" : "user-" + idx}
                   onClick={(e) => handleMenu(e, workspace.id)}
                   onMouseOver={(e) => setShowPoints(true)}
-                  onMouseOut={(e) => setShowPoints(false)}
                   onContextMenu={handleContextMenu}
                 >
                   <div className="menu-work-name" onClick={navegacion}>
@@ -194,7 +193,7 @@ const Menu = ({ filteredWorkspaces }) => {
                       ref={menuRef}
                     >
                       <svg
-                     /*    xmlns="http://www.w3.org/2000/svg"
+                        /*    xmlns="http://www.w3.org/2000/svg"
                         width="100"
                         height="100" */
                         viewBox="0 0 100 100"
@@ -204,7 +203,7 @@ const Menu = ({ filteredWorkspaces }) => {
                         <circle cx="75" cy="50" r="5" fill="black" />
                       </svg>
                     </div>
-                  )}{" "}
+                  )}
                   {isSelected[workspace.id] && isOpen && (
                     <div
                       className="menu-workspace-menu-workspace-settings"
