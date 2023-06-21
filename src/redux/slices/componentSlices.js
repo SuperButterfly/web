bb1f30063f24a53993bdae1019b61cddb1f92089
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-06-21 14:09:48 -0300

    added redux action getParentID

--- src/redux/slices/componentSlices.js
+++ src/redux/slices/componentSlices.js
@@ -1,37 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const componentSlices = createSlice({
-  name: 'components',
+  name: "components",
   initialState: {
     componentSelected: {},
-    componentsSelected:[],
-    width: 0
+    componentsSelected: [],
+    width: 0,
   },
   reducers: {
     setSelectedComponent(state, actions) {
       state.componentSelected = actions.payload;
-      state.componentsSelected = [actions.payload]
+      state.componentsSelected = [actions.payload];
     },
     createNewComponent(state, actions) {
       state.component = actions.payload;
     },
     updateSelectedComponent(state, actions) {
       state.componentSelected = actions.payload;
-      state.componentsSelected=[actions.payload];
+      state.componentsSelected = [actions.payload];
     },
     updateWidth(state, actions) {
       state.width = actions.payload;
     },
-    setComponentsSelected(state,actions){
-      state.componentsSelected = [...state.componentsSelected,actions.payload];
+    setComponentsSelected(state, actions) {
+      state.componentsSelected = [...state.componentsSelected, actions.payload];
     },
-    updateComponentsSelected(state,actions){
-      state.componentsSelected=actions.payload
-    cleanComponentsSelected(state,actions){
-      state.componentsSelected=[]
-    }
-  }
+    updateComponentsSelected(state, actions) {
+      state.componentsSelected = actions.payload;
+    },
+    cleanComponentsSelected(state, actions) {
+      state.componentsSelected = [];
+    },
+  },
 });
 
-export const { setSelectedComponent,setComponentsSelected, createNewComponent, updateSelectedComponent,cleanComponentsSelected, updateWidth } = componentSlices.actions;
+export const {
+  setSelectedComponent,
+  setComponentsSelected,
+  createNewComponent,
+  updateSelectedComponent,
+  cleanComponentsSelected,
+  updateWidth,
+} = componentSlices.actions;
 export default componentSlices.reducer;
