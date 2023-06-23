51f1d8a7d97ac2e2807e91be6554f8bdb7a1322c
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-06-23 10:23:29 -0300

    added slice for EditingId state

--- src/redux/slices/componentSlices.js
+++ src/redux/slices/componentSlices.js
@@ -1,41 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const componentSlices = createSlice({
-  name: 'components',
+  name: "components",
   initialState: {
     componentSelected: {},
-    componentsSelected:[],
-    width: 0
+    componentsSelected: [],
+    width: 0,
+    editingId: null,
   },
   reducers: {
     setSelectedComponent(state, actions) {
       state.componentSelected = actions.payload;
-      state.componentsSelected = [actions.payload]
+      state.componentsSelected = [actions.payload];
     },
     createNewComponent(state, actions) {
       state.component = actions.payload;
     },
     updateSelectedComponent(state, actions) {
       state.componentSelected = actions.payload;
-      state.componentsSelected=[actions.payload];
+      state.componentsSelected = [actions.payload];
     },
     updateWidth(state, actions) {
       state.width = actions.payload;
     },
-    setComponentsSelected(state,actions){
-      state.componentsSelected = [...state.componentsSelected,actions.payload];
+    setComponentsSelected(state, actions) {
+      state.componentsSelected = [...state.componentsSelected, actions.payload];
     },
-    resetAndSetComponentsSelected(state,actions){
+    resetAndSetComponentsSelected(state, actions) {
       state.componentsSelected = actions.payload;
     },
-    updateComponentsSelected(state,actions){
-      state.componentsSelected=actions.payload
+    updateComponentsSelected(state, actions) {
+      state.componentsSelected = actions.payload;
     },
-    cleanComponentsSelected(state,actions){
-      state.componentsSelected=[]
-    }
-  }
+    cleanComponentsSelected(state, actions) {
+      state.componentsSelected = [];
+    },
+    setEditingId(state, actions) {
+      state.editingId = actions.payload;
+    },
+  },
 });
 
-export const { setSelectedComponent,setComponentsSelected, createNewComponent, updateSelectedComponent,cleanComponentsSelected, updateWidth,resetAndSetComponentsSelected  } = componentSlices.actions;
+export const {
+  setSelectedComponent,
+  setComponentsSelected,
+  createNewComponent,
+  updateSelectedComponent,
+  cleanComponentsSelected,
+  updateWidth,
+  resetAndSetComponentsSelected,
+  setEditingId,
+} = componentSlices.actions;
 export default componentSlices.reducer;
