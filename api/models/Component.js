659d7dbaf7e537db090aa8bd16cbe61a552962e1
Pablo Spector <pablo_spector@hotmail.com>
2023-06-23 03:53:16 +0000

    correccion a modelo component

--- api/models/Component.js
+++ api/models/Component.js
@@ -49,9 +49,9 @@ module.exports = (sequelize) =>
     isDeleted: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
-    } ,
+    } /* ,
     isShow: {
       type: DataTypes.BOOLEAN,
       defaultValue: true,
-    },
+    }, */
   });
