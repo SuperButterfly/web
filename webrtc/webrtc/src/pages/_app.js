39c7be85ba6a61f966b6d1f4d42f28df99bdd6d1
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-06-24 14:17:41 -0300

    movi WebRTC fuera del repo

--- webrtc/webrtc/src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "@component/styles/globals.css";
-import React from "react";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
--- webrtc/webrtc/src/pages/testTraductor.js
+++
@@ -1,3 +0,0 @@
-const {traducirAReactNative} = require("./traductorFunction");
-
-traducirAReactNative()
