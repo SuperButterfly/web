65431641b9b4d5b70ae555d8e4ade3f7748d68e1
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-06-24 10:59:25 -0300

    translations input routes

---
+++ api/routes/inputs/translation.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const { Router } = require("express");
+const translationRouter = Router();
+const { addFile } = require("../../controllers/translation.controllers.js");
+
+translationRouter.post("/addFile", addFile);
+
+module.exports = translationRouter;
