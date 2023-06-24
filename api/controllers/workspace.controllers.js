a77e91623bb50fb0db1528a2845a6ebe401a3d97
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-06-24 15:21:12 -0300

    fix files

--- api/controllers/workspace.controllers.js
+++ api/controllers/workspace.controllers.js
@@ -1,10 +1,5 @@
-const {
-  Workspace,
-  Template,
-  Component,
-  User
-} = require("../database.js");
-const componentsList = require('./toCreate.js');
+const { Workspace, Template, Component, User } = require("../database.js");
+const componentsList = require("./toCreate.js");
 
 const addWorkspace = async (req, res, next) => {
   const userEmail = req.params.userEmail;
@@ -12,43 +7,45 @@ const addWorkspace = async (req, res, next) => {
     const userFound = await User.findOne({
       where: {
         email: userEmail,
-        isDeleted: false
-      }
-    })
-    if (!userFound) throw new Error('User not found or banned')
-    const homepage = await Component.create(componentsList['Home'])
+        isDeleted: false,
+      },
+    });
+    if (!userFound) throw new Error("User not found or banned");
+    const homepage = await Component.create(componentsList["Home"]);
     const newTemplate = await Template.create();
-    const newWorkspace = await Workspace.create()
+    const newWorkspace = await Workspace.create();
 
-    await newTemplate.addPages(homepage)
-    await newWorkspace.addProjects(newTemplate)
-    await userFound.addWorkspaces(newWorkspace)
+    await newTemplate.addPages(homepage);
+    await newWorkspace.addProjects(newTemplate);
+    await userFound.addWorkspaces(newWorkspace);
 
     const userUpdated = await User.findOne({
       where: {
         email: userEmail,
-        isDeleted: false
+        isDeleted: false,
       },
       include: {
         model: Workspace,
-        as: 'workspaces',
+        as: "workspaces",
         include: {
           model: Template,
-          as: 'projects',
-          include: [{
-            model: Component,
-            as: 'pages',
-          }, {
-            model: Component,
-            as: 'components',
-          }]
-        }
-      }
-    })
+          as: "projects",
+          include: [
+            {
+              model: Component,
+              as: "pages",
+            },
+            {
+              model: Component,
+              as: "components",
+            },
+          ],
+        },
+      },
+    });
 
     res.json({ workspaces: userUpdated.workspaces });
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
@@ -58,91 +55,120 @@ const getWorkspace = async (req, res, next) => {
   try {
     const workspace = await Workspace.findOne({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
-      include: [{
-        model: Template,
-        as: 'projects',
-        include: [{
-          model: Component,
-          as: 'pages',
-        }, {
-          model: Component,
-          as: 'components',
-        }]
-      }, {
-        model: User,
-        as: "collaboratorviewer"
-      }, {
-        model: User,
-        as: "collaboratoreditor"
-      }],
-      order:[
-        [{model:Template,as:"projects"},{model:Component, as:"pages"},'name','ASC'],
-        [{model:Template,as:"projects"},{model:Component, as:"components"},'name','ASC']
-      ]
-    })
+      include: [
+        {
+          model: Template,
+          as: "projects",
+          include: [
+            {
+              model: Component,
+              as: "pages",
+            },
+            {
+              model: Component,
+              as: "components",
+            },
+          ],
+        },
+        {
+          model: User,
+          as: "collaboratorviewer",
+        },
+        {
+          model: User,
+          as: "collaboratoreditor",
+        },
+      ],
+      order: [
+        [{ model: Template, as: "projects" }, { model: Component, as: "pages" }, "name", "ASC"],
+        [
+          { model: Template, as: "projects" },
+          { model: Component, as: "components" },
+          "name",
+          "ASC",
+        ],
+      ],
+    });
 
     if (!workspace) {
       throw new Error("Workspace not found or removed");
     }
 
-    res.json({ workspace })
-  }
-  catch (error) {
+    res.json({ workspace });
+  } catch (error) {
     return next(error);
   }
 };
 
-// getUserWorkspaces x userEmail   x params 
+// getUserWorkspaces x userEmail   x params
 const getUserWorkspaces = async (req, res, next) => {
   try {
-
     const userFound = await User.findOne({
       where: {
         email: req.params.userEmail,
-        isDeleted: false
+        isDeleted: false,
       },
-      include: [{
-        model: Workspace,
-        as: 'workspaces',
-        //where: {
-        //isDeleted: false
-        //},
-        include: {
-          model: Template,
-          as: 'projects',
-          //order: [['name', 'DESC']],
-          // where: {
-          //   isDeleted: false
-          // },
-          include: [{
-            model: Component,
-            as: 'pages',
-            // order: [['name', 'DESC']]
+      include: [
+        {
+          model: Workspace,
+          as: "workspaces",
+          //where: {
+          //isDeleted: false
+          //},
+          include: {
+            model: Template,
+            as: "projects",
+            //order: [['name', 'DESC']],
             // where: {
             //   isDeleted: false
             // },
-          }, {
-            model: Component,
-            as: 'components',
-            //order: [['name', 'DESC']]
-            // where: {
-            //   isDeleted: false
-            // },
-          }]
-        }
-      }, {
-        model: User,
-        as: "collaboratorviewer"
-      }, {
-        model: User,
-        as: "collaboratoreditor"
-      }],
-      order:[
-        [{model:Workspace,as:"workspace"},{model:Template,as:"projects"},{model:Component, as:"pages"},'name','ASC'],
-        [{model:Workspace,as:"workspace"},{model:Template,as:"projects"},{model:Component, as:"components"},'name','ASC']
-      ]
+            include: [
+              {
+                model: Component,
+                as: "pages",
+                // order: [['name', 'DESC']]
+                // where: {
+                //   isDeleted: false
+                // },
+              },
+              {
+                model: Component,
+                as: "components",
+                //order: [['name', 'DESC']]
+                // where: {
+                //   isDeleted: false
+                // },
+              },
+            ],
+          },
+        },
+        {
+          model: User,
+          as: "collaboratorviewer",
+        },
+        {
+          model: User,
+          as: "collaboratoreditor",
+        },
+      ],
+      order: [
+        [
+          { model: Workspace, as: "workspace" },
+          { model: Template, as: "projects" },
+          { model: Component, as: "pages" },
+          "name",
+          "ASC",
+        ],
+        [
+          { model: Workspace, as: "workspace" },
+          { model: Template, as: "projects" },
+          { model: Component, as: "components" },
+          "name",
+          "ASC",
+        ],
+      ],
     });
 
     if (!userFound) {
@@ -153,27 +179,23 @@ const getUserWorkspaces = async (req, res, next) => {
     }
 
     res.json({ workspaces: userFound.workspaces });
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
 
-// updateWorkspace  x id  x params 
+// updateWorkspace  x id  x params
 const updateWorkspace = async (req, res, next) => {
   try {
-    await Workspace.update(
-      req.body, {
-        where: {
-          id: req.params.id
-        }
-      }
-    );
+    await Workspace.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
 
     const workspace = await retrieveWorkspace(req.params.id);
     res.json({ workspace });
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
@@ -182,27 +204,25 @@ const addCollaborator = async (req, res, next) => {
   try {
     const workspaceFound = await Workspace.findByPk(req.params.id);
     if (!workspaceFound) {
-      throw new Error('Workspace not found')
+      throw new Error("Workspace not found");
     }
 
     const userFound = await User.findOne({
-      where: { email: req.body.email }
-    })
+      where: { email: req.body.email },
+    });
     if (!userFound) {
-      throw new Error('User not found')
+      throw new Error("User not found");
     }
 
     if (req.query.collaborator === "viewer") {
-      await workspaceFound.addCollaboratorviewer(userFound)
-    }
-    else {
-      await workspaceFound.addCollaboratoreditor(userFound)
+      await workspaceFound.addCollaboratorviewer(userFound);
+    } else {
+      await workspaceFound.addCollaboratoreditor(userFound);
     }
 
     const workspace = await retrieveWorkspace(req.params.id);
     res.json({ workspace });
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
@@ -211,29 +231,27 @@ const updateCollaborator = async (req, res, next) => {
   try {
     const workspaceFound = await Workspace.findByPk(req.params.id);
     if (!workspaceFound) {
-      throw new Error('Workspace not found')
+      throw new Error("Workspace not found");
     }
 
     const userFound = await User.findOne({
-      where: { email: req.body.email }
-    })
+      where: { email: req.body.email },
+    });
     if (!userFound) {
-      throw new Error('User not found')
+      throw new Error("User not found");
     }
 
     if (req.query.collaborator === "viewer") {
-      await workspaceFound.removeCollaboratoreditor(userFound)
-      await workspaceFound.addCollaboratorviewer(userFound)
-    }
-    else {
-      await workspaceFound.removeCollaboratorviewer(userFound)
-      await workspaceFound.addCollaboratoreditor(userFound)
+      await workspaceFound.removeCollaboratoreditor(userFound);
+      await workspaceFound.addCollaboratorviewer(userFound);
+    } else {
+      await workspaceFound.removeCollaboratorviewer(userFound);
+      await workspaceFound.addCollaboratoreditor(userFound);
     }
 
     const workspace = await retrieveWorkspace(req.params.id);
     res.json({ workspace });
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
@@ -242,23 +260,22 @@ const removeCollaborator = async (req, res, next) => {
   try {
     const workspaceFound = await Workspace.findByPk(req.params.id);
     if (!workspaceFound) {
-      throw new Error('Workspace not found')
+      throw new Error("Workspace not found");
     }
 
     const userFound = await User.findOne({
-      where: { email: req.body.email }
-    })
+      where: { email: req.body.email },
+    });
     if (!userFound) {
-      throw new Error('User not found')
+      throw new Error("User not found");
     }
 
-    workspaceFound.removeCollaboratoreditor(userFound)
-    workspaceFound.removeCollaboratorviewer(userFound)
+    workspaceFound.removeCollaboratoreditor(userFound);
+    workspaceFound.removeCollaboratorviewer(userFound);
 
     const workspace = await retrieveWorkspace(req.params.id);
     res.json({ workspace });
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
@@ -272,37 +289,43 @@ const deleteWorkspace = async (req, res, next) => {
     await workspace.save();
 
     res.json({});
-  }
-  catch (error) {
+  } catch (error) {
     return next(error);
   }
 };
 
 const retrieveWorkspace = async (id) => {
   return await Workspace.findByPk(id, {
-      include: [{
+    include: [
+      {
         model: Template,
-        as: 'projects',
-        include: [{
-          model: Component,
-          as: 'pages',
-        }, {
-          model: Component,
-          as: 'components',
-        }]
-      }, {
+        as: "projects",
+        include: [
+          {
+            model: Component,
+            as: "pages",
+          },
+          {
+            model: Component,
+            as: "components",
+          },
+        ],
+      },
+      {
         model: User,
-        as: "collaboratorviewer"
-      }, {
+        as: "collaboratorviewer",
+      },
+      {
         model: User,
-        as: "collaboratoreditor"
-      }],
-      order:[
-        [{model:Template,as:"projects"},{model:Component, as:"pages"},'name','ASC'],
-        [{model:Template,as:"projects"},{model:Component, as:"components"},'name','ASC']
-      ]
-    })
-}
+        as: "collaboratoreditor",
+      },
+    ],
+    order: [
+      [{ model: Template, as: "projects" }, { model: Component, as: "pages" }, "name", "ASC"],
+      [{ model: Template, as: "projects" }, { model: Component, as: "components" }, "name", "ASC"],
+    ],
+  });
+};
 
 module.exports = {
   addWorkspace,
@@ -312,5 +335,5 @@ module.exports = {
   addCollaborator,
   updateCollaborator,
   removeCollaborator,
-  deleteWorkspace
+  deleteWorkspace,
 };
