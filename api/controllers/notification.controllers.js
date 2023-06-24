0800a29bde982862c798f0a58aaeabf7346d1797
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-06-24 13:59:39 -0300

    modularizacion de back web, division de controllers y routers con formato input y output

--- api/controllers/notification.controllers.js
+++ api/controllers/notification.controllers.js
@@ -1,16 +1,14 @@
 const { deleteNotification } = require("../utils/notification.js");
 
-
 const deleteNotificationSaved = async (req, res, next) => {
-  const id = req.params.id
-   
-   try{ 
-    const notificationDeleted = await deleteNotification(id)
-    res.send(notificationDeleted)
-   }
-   catch(e){
-     res.send(e.message)  
-   }
-}
+  const id = req.params.id;
 
-module.exports = {deleteNotificationSaved}
+  try {
+    const notificationDeleted = await deleteNotification(id);
+    res.send(notificationDeleted);
+  } catch (e) {
+    res.send(e.message);
+  }
+};
+
+module.exports = { deleteNotificationSaved };
