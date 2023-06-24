edd6d0d2b00142afffde0b5a4a8fca88ceb2f89d
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-06-24 14:03:11 -0300

    modularizacion de back de WEB, en formato input y output

--- api/routes/outputs/class.routes.js
+++ api/routes/outputs/class.routes.js
@@ -1,5 +1,4 @@
-"use strict";
-const { verifyToken } = require("../../middlewares/auth.js");
+// const { verifyToken } = require("../../middlewares/auth.js");
 const { Router } = require("express");
 
 const classRouter = Router();
