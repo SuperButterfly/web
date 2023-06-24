55d879fd06049b04ae0c3a34efad02b8669a217b
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-06-24 13:59:33 -0300

    modularizacion de back web, division de controllers y routers con formato input y output

--- api/controllers/database.controller.js
+++ api/controllers/database.controller.js
@@ -1,73 +1,70 @@
 //const { next } = require("cheerio/lib/api/traversing.js");
-const { Template, Component } = require("../database.js");
+const { Template } = require("../database.js");
 
-const getDatabaseByProject = async(req,res,next)=>{
-    try{
-        if (!req.params.idProject)
-            throw new Error("idProject is required") 
-        const project = await Template.findByPk(req.params.idProject)
+const getDatabaseByProject = async (req, res, next) => {
+  try {
+    if (!req.params.idProject) throw new Error("idProject is required");
+    const project = await Template.findByPk(req.params.idProject);
 
-        if(!project)
-            throw new Error("Project not found")
-        res.send(project.databases)
-    }catch(error){
-        return next(error)
-    }
-}
+    if (!project) throw new Error("Project not found");
+    res.send(project.databases);
+  } catch (error) {
+    return next(error);
+  }
+};
 
 //const getDatabasesAllProjects = async(req,res)
 
-const postDatabaseToProject = async (req,res,next)=>{
-    try{
-        const {idProject}=req.params
-        if (!idProject)
-            throw new Error("idProject is required") 
-        
-        const {databases} = req.body;
+const postDatabaseToProject = async (req, res, next) => {
+  try {
+    const { idProject } = req.params;
+    if (!idProject) throw new Error("idProject is required");
 
-        if(!databases)
-            throw new Error("Databases are required")
+    const { databases } = req.body;
 
-        const project = await Template.update({
-            databases
-        },{
-            where:{
-                id:idProject
-            }
-        })
-        if(!project)
-            throw new Error("Project not found")
-        res.send('ok')
-    }catch(error){
-        return next(error)
-    }
-}
+    if (!databases) throw new Error("Databases are required");
 
-const deleteDatabasesToProjects = async (req,res)=>{
-    try{
-        const {idProject} = req.params
-        if (!idProject)
-            throw new Error("idProject is required") 
-        
-        const project = await Template.update({
-            databases:[]
-        },{
-            where:{
-                id:idProject
-            }
-        })
-        if(!project)
-            throw new Error("Project not found")
-        res.send(project)
-    }catch(error){
-        return next(error)
-    }
-}
+    const project = await Template.update(
+      {
+        databases,
+      },
+      {
+        where: {
+          id: idProject,
+        },
+      }
+    );
+    if (!project) throw new Error("Project not found");
+    res.send("ok");
+  } catch (error) {
+    return next(error);
+  }
+};
 
+const deleteDatabasesToProjects = async (req, res, next) => {
+  try {
+    const { idProject } = req.params;
+    if (!idProject) throw new Error("idProject is required");
 
+    const project = await Template.update(
+      {
+        databases: [],
+      },
+      {
+        where: {
+          id: idProject,
+        },
+      }
+    );
+    if (!project) throw new Error("Project not found");
+    res.send(project);
+  } catch (error) {
+    return next(error);
+  }
+};
 
-module.exports={
-    getDatabaseByProject,
-    postDatabaseToProject,
-    deleteDatabasesToProjects
-}
+module.exports = {
+  getDatabaseByProject,
+  postDatabaseToProject,
+  deleteDatabasesToProjects,
+};
