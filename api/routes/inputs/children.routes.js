61d077c1ba08a940ddfd89043f1f42be7a8217c0
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-06-24 10:53:26 -0300

    children  input and output routes

---
+++ api/routes/inputs/children.routes.js
@@ -0,0 +1,25 @@
+"use strict";
+const {
+  addSection,
+  addChildren,
+  updateChildren,
+  deleteChildrenId,
+} = require("../../controllers/children.controllers.js");
+const { verifyToken } = require("../../middlewares/auth.js");
+
+const { Router } = require("express");
+const childrenRouter = Router();
+
+//  post  /:id addChildren  complex
+childrenRouter.post("/tree/:componentId", [verifyToken], addSection);
+
+// post  /:id addChildren  simple
+childrenRouter.post("/:componentId", [verifyToken], addChildren);
+
+// patch  /:id  updateChildren
+childrenRouter.patch("/:id", [verifyToken], updateChildren);
+
+// patch  /delete/:id  deleteChildrenId
+childrenRouter.patch("/delete/:id", [verifyToken], deleteChildrenId);
+
+module.exports = childrenRouter;
