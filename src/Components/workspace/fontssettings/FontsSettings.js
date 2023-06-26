e3fa01d6adbe164427e758aa63ed259f8ca08234
Mariana <mariana.stocco@outlook.com>
2023-06-26 18:17:46 -0300

    26/06-FontsSettings Styles

--- src/Components/workspace/fontssettings/FontsSettings.js
+++ src/Components/workspace/fontssettings/FontsSettings.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const FontsSettings = () => {
   const [data, setData] = useState({
     name: 'Custom name',
-    url: 'Custom font url',
+    url: '',
     font1: 'Georgia',
     font2: 'Impact',
     font3: 'Inter',
@@ -22,6 +22,8 @@ const FontsSettings = () => {
   const [selectedFont, setSelectedFont] = useState(null);
   const [selectedOption, setSelectedOption] = useState('');
   const [jsonData, setJsonData] = useState([]);
+  const [isFontSelected, setIsFontSelected] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,16 +31,17 @@ const FontsSettings = () => {
         const response = await fetch('/data.json');
         const jsonData = await response.json();
         setJsonData(jsonData.data); // Accede al array dentro del objeto JSON
-        console.log(jsonData)
+        console.log(jsonData);
       } catch (error) {
         console.log('Error al cargar el archivo JSON:', error);
       }
     };
     fetchData();
-  }, [])
+  }, []);
 
   const handleSelectFont = (event) => {
     setSelectedOption(event.target.value);
+    setIsFontSelected(true);
   };
 
   const handleFontChange = ev => {
@@ -55,6 +58,7 @@ const FontsSettings = () => {
         });
         if (matchingFont) {
           setFontName(matchingFont.family);
+          setSelectedOption(matchingFont.family);
         } else {
           setFontName('Unknown Font');
         }
@@ -79,12 +83,13 @@ const FontsSettings = () => {
   const handleNameChange = ev => {
     const updatedName = ev.target.value;
     setFontName(updatedName);
-    setSelectedOption(ev.target.value)
+    setSelectedOption(ev.target.value);
   };
 
   const handleFontFileChange = ev => {
     const file = ev.target.files[0];
     setFontFile(file);
+    setFontName(file.name);
   };
 
   const handleSubmit = ev => {
@@ -98,15 +103,17 @@ const FontsSettings = () => {
 
       // Agregar la fuente seleccionada a la lista de fuentes activas
       const newFont = {
-        name: selectedOptionObject ? selectedOptionObject.family : 'Unknown Font',
+        name: selectedOptionObject ? selectedOptionObject.family : fontNameValue,
         url: fontUrl
       };
       setSelectedFonts(prevFonts => [...prevFonts, newFont]);
       setFontName('');
+      setIsFontSelected(false);
+      setShowError(false);
+    } else {
+      setShowError(true);
     }
   };
-
-
 
   const handleDeleteFont = index => {
     setSelectedFonts(prevFonts => prevFonts.filter((_, i) => i !== index));
@@ -118,26 +125,26 @@ const FontsSettings = () => {
   return (
     <div className="fonts-settings-container">
       <div className="fonts-settings-name-container">
-        <span className="fonts-settings-nametitle">Name</span>
+        <span className="projects-settings-title">Name</span>
         <span className="fonts-settings-name-text">
-          Name to be displayed in the Font list
+          Name to be displayed in the Font list:
         </span>
         <div className="fonts-settings-name-input">
           <input type="text" id="name" className="fonts-settings-name" value={fontName} onChange={handleNameChange} />
         </div>
       </div>
       <div className="fonts-settings-url-container">
-        <span className="fonts-settings-heading-url">Font URL</span>
-        <span className="fonts-settings-url-text1">
+        <span className="projects-settings-title">Font URL:</span>
+        <span className="fonts-settings-name-text ">
           Custom fonts will be available after they are added.
         </span>
         <span className="fonts-settings-url-text2">
           Adding Google Fonts can be done from inside your project.
         </span>
-        <input type="text" id="url" className="fonts-settings-url" value={data.url} onChange={handleFontChange} />
-        <input type="file" id="font-file" accept=".ttf,.otf" onChange={handleFontFileChange} />
+        <input type="text" id="url" className="fonts-settings-url" placeholder='copy and paste a font url' value={data.url} onChange={handleFontChange} />
+        <input className="fonts-settings-add-button button" type="file" id="font-file" accept=".ttf,.otf" onChange={handleFontFileChange} />
         <span>Or you can pick one of our available fonts.</span>
-        <select value={selectedOption} onChange={handleSelectFont}>
+        <select className="fonts-settings-url" value={selectedOption} onChange={handleSelectFont}>
           {jsonData.map((option) => (
             <option key={option.id} value={option.id}>
               {option.family}
@@ -148,7 +155,11 @@ const FontsSettings = () => {
       <button className="fonts-settings-add-button button" onClick={handleSubmit}>
         Add Font
       </button>
+      {showError && (
+        <div className="error-line"> <span>some fields are empty</span> </div>
+      )}
       <div className="fonts-settings-container1">
+        <h2 className="projects-settings-title">Font List</h2>
         <span className="fonts-settings-activefonts">Active fonts</span>
         <span className="fonts-settings-font1">{data.font1}</span>
         <span className="fonts-settings-font2">{data.font2}</span>
