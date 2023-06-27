c49cec6eacead15946fc3c5d41aca3c3ff1fd126
Benjamin Castro <benyamin0714@gmail.com>
2023-06-27 16:59:21 -0300

    refactoreo

--- src/Pages/dashboard/Editor/dataTablesScreen/History/History.jsx
+++ src/Pages/dashboard/Editor/dataTablesScreen/History/History.jsx
@@ -22,10 +22,7 @@ const versionesPorDia = (versions) => {
         grupoVersiones["Hoy"] = [version];
       }
     } else if (isBefore(new Date(date), subWeeks(new Date(), 1))) {
-      const formattedFullDate = format(
-        new Date(date),
-        "dd 'de' MMMM 'del' yyyy"
-      );
+      const formattedFullDate = format(new Date(date), "dd 'de' MMMM 'del' yyyy");
       if (grupoVersiones[formattedFullDate]) {
         grupoVersiones[formattedFullDate].push(version);
       } else {
@@ -48,58 +45,56 @@ const VersionHistory = ({ currentVersion, onVersionSelect }) => {
   const [versions, setVersions] = useState([]);
   const [search, setSearch] = useState("");
 
-
   const syncedData = [
-        {
-          id: 1,
-          name: "Versión 1",
-          description: "Descripción de la versión 1",
-          date: new Date(2023, 5, 22),
-          time: "10:00 AM",
-          author: "Fiorella",
-        },
-        {
-          id: 2,
-          name: "Versión 2",
-          description: "Descripción de la versión 2",
-          date: new Date(2023, 5, 20),
-          time: "02:30 PM",
-          author: "Aythen",
-        },
-        {
-          id: 3,
-          name: "Versión 3",
-          description: "Descripción de la versión 3",
-          date: new Date(2023, 5, 20),
-          time: "10:56 AM",
-          author: "Pablo",
-        },
-        {
-          id: 4,
-          name: "Versión 4",
-          description: "Descripción de la versión 4",
-          date: new Date(2023, 5, 20),
-          time: "08:45 PM",
-          author: "Agustin",
-        },
-        {
-          id: 5,
-          name: "Versión 5",
-          description: "Descripción de la versión 5",
-          date: new Date(2023, 5, 19),
-          time: "12:00 AM",
-          author: "Fiorella",
-        },
-        {
-          id: 6,
-          name: "Versión 6",
-          description: "Descripción de la versión 6",
-          date: new Date(2023, 5, 10),
-          time: "02:35 AM",
-          author: "Aythen",
-        },
-      ];
-
+    {
+      id: 1,
+      name: "Versión 1",
+      description: "Descripción de la versión 1",
+      date: new Date(2023, 5, 22),
+      time: "10:00 AM",
+      author: "Fiorella",
+    },
+    {
+      id: 2,
+      name: "Versión 2",
+      description: "Descripción de la versión 2",
+      date: new Date(2023, 5, 20),
+      time: "02:30 PM",
+      author: "Aythen",
+    },
+    {
+      id: 3,
+      name: "Versión 3",
+      description: "Descripción de la versión 3",
+      date: new Date(2023, 5, 20),
+      time: "10:56 AM",
+      author: "Pablo",
+    },
+    {
+      id: 4,
+      name: "Versión 4",
+      description: "Descripción de la versión 4",
+      date: new Date(2023, 5, 20),
+      time: "08:45 PM",
+      author: "Agustin",
+    },
+    {
+      id: 5,
+      name: "Versión 5",
+      description: "Descripción de la versión 5",
+      date: new Date(2023, 5, 19),
+      time: "12:00 AM",
+      author: "Fiorella",
+    },
+    {
+      id: 6,
+      name: "Versión 6",
+      description: "Descripción de la versión 6",
+      date: new Date(2023, 5, 10),
+      time: "02:35 AM",
+      author: "Aythen",
+    },
+  ];
 
   useEffect(() => {
     setVersions(syncedData);
@@ -114,89 +109,120 @@ const VersionHistory = ({ currentVersion, onVersionSelect }) => {
   };
 
   const filtroVersiones = versions.filter((version) =>
-    Object.values(version)
-      .join(" ")
-      .toLowerCase()
-      .includes(search.toLowerCase())
+    Object.values(version).join(" ").toLowerCase().includes(search.toLowerCase())
   );
 
   const grupoVersiones = versionesPorDia(filtroVersiones);
 
   return (
-    <div className={styles.versionHistoryContainer}>
-      <div className={styles.versionHistoryMenu}>
-        <div
-          className={`select ${isMenuOpen ? "open" : ""}`}
-          onClick={handleMenuToggle}
-        >
-          <div className={styles.selectedOption}>
-            <span className={styles.arrow}>
-              {isMenuOpen
-                ? "▲ Historial de versiones"
-                : "▼ Historial de versiones"}
-            </span>
-            <span className={styles.versionName}>
-              {currentVersion !== null
-                ? versions.find((v) => v.id === currentVersion)?.name
-                : "Historial de versiones"}
-            </span>
-          </div>
-          <div className="options">
-            <div className={styles.searchBar}>
-              <input
-                type="text"
-                placeholder="Buscar versión..."
-                value={search}
-                onChange={handleSearch}
-                className="search"
-              />
-            </div>
-            {isMenuOpen && (
-              <div>
-                <hr></hr>
-
-                {Object.entries(grupoVersiones).map(([date, versions]) => (
-                  <div key={date} className={styles.versionGrupo}>
-                    <div className={styles.dateHeadin}>
-                      {date === "Hoy"
-                        ? "Hoy"
-                        : date === "Ayer"
-                        ? "Ayer"
-                        : date}
-                    </div>
-                    {versions.map((version) => (
-                      <div
-                        key={version.id}
-                        className={`option ${
-                          currentVersion === version.id ? "selected" : ""
-                        }`}
-                        onClick={() => onVersionSelect(version.id)}
-                      >
-                        <div className={styles.versionInfo}>
-                          <span className={styles.versionDetails}>
-                          <span className={styles.versionDate}>{format(version.date, "dd MMMM yyyy")},</span>
-
-                            
-                            {/* </span> */}
-                            <span className={styles.versionTime}>{version.time}</span>
-                            <span className={styles.versionAuthor}>
-                              por {version.author}
-                            </span>
-                          </span>
-                        </div>
-                        <div className={styles.versionDescription}>
-                          ◾{version.description}
-                        </div>
-                      </div>
-                    ))}
+    <div className={styles.container}>
+      <div onClick={handleMenuToggle} className={styles.subContainerAndTitles}>
+        <div className={styles.title}>
+          {isMenuOpen ? "▲ Historial de versiones ▲" : "▼ Historial de versiones ▼"}
+          {/* <span>
+            {currentVersion !== null
+              ? versions.find((v) => v.id === currentVersion)?.name
+              : "Historial de versiones"}
+          </span> */}
+        </div>
+        {isMenuOpen && (
+          <div className={styles.containerEachVersions}>
+            {Object.entries(grupoVersiones).map(([date, versions]) => (
+              <div key={date} className={styles.dropDownVersions}>
+                <span className={styles.font}>
+                  {date === "Hoy" ? "Hoy" : date === "Ayer" ? "Ayer" : date}
+                </span>
+                {versions.map((version) => (
+                  <div
+                    key={version.id}
+                    className={styles.dateVersions}
+                    onClick={() => onVersionSelect(version.id)}
+                  >
+                    <span>
+                      {format(version.date, "dd MMMM yyyy")}, {version.time} por {version.author}
+                    </span>
+                    <p style={{ margin: 0 }}> ◾{version.description} </p>
                   </div>
                 ))}
               </div>
-            )}
+            ))}
           </div>
-        </div>
+        )}
       </div>
     </div>
+    // <div className={styles.versionHistoryContainer}>
+    //   <div className={styles.versionHistoryMenu}>
+    //     <div
+    //       className={`select ${isMenuOpen ? "open" : ""}`}
+    //       onClick={handleMenuToggle}
+    //     >
+    //       <div className={styles.selectedOption}>
+    //         <span className={styles.arrow}>
+    //           {isMenuOpen
+    //             ? "▲ Historial de versiones"
+    //             : "▼ Historial de versiones"}
+    //         </span>
+
+    //         <span className={styles.versionName}>
+    //           {currentVersion !== null
+    //             ? versions.find((v) => v.id === currentVersion)?.name
+    //             : "Historial de versiones"}
+    //         </span>
+    //       </div>
+    //       <div className="options">
+    //         <div className={styles.searchBar}>
+    //           <input
+    //             type="text"
+    //             placeholder="Buscar versión..."
+    //             value={search}
+    //             onChange={handleSearch}
+    //             className="search"
+    //           />
+    //         </div>
+    //         {isMenuOpen && (
+    //           <div>
+    //             <hr></hr>
+    //             {Object.entries(grupoVersiones).map(([date, versions]) => (
+    //               <div key={date} className={styles.versionGrupo}>
+    //                 <div className={styles.dateHeadin}>
+    //                   {date === "Hoy"
+    //                     ? "Hoy"
+    //                     : date === "Ayer"
+    //                     ? "Ayer"
+    //                     : date}
+    //                 </div>
+    //                 {versions.map((version) => (
+    //                   <div
+    //                     key={version.id}
+    //                     className={`option ${
+    //                       currentVersion === version.id ? "selected" : ""
+    //                     }`}
+    //                     onClick={() => onVersionSelect(version.id)}
+    //                   >
+    //                     <div className={styles.versionInfo}>
+    //                       <span className={styles.versionDetails}>
+    //                       <span className={styles.versionDate}>{format(version.date, "dd MMMM yyyy")},</span>
+
+    //                         {/* </span> */}
+    //                         <span className={styles.versionTime}>{version.time}</span>
+    //                         <span className={styles.versionAuthor}>
+    //                           por {version.author}
+    //                         </span>
+    //                       </span>
+    //                     </div>
+    //                     <div className={styles.versionDescription}>
+    //                       ◾{version.description}
+    //                     </div>
+    //                   </div>
+    //                 ))}
+    //               </div>
+    //             ))}
+    //           </div>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
