634f7fdeb0a48fcb77ed2ed7b56937c3fc04634e
Martín Araujo <mnaheavy@gmail.com>
2023-06-27 20:48:14 -0300

    Pressets config

--- src/Pages/dashboard/Editor/pressets/ColorGroup.js
+++ src/Pages/dashboard/Editor/pressets/ColorGroup.js
@@ -1,42 +1,45 @@
+import "./color-group.css";
+import { useState } from "react";
+import ColorToken from "./ColorToken";
+import ColorMenu from "./ColorMenu";
+import data from "./color.json";
 
-import './color-group.css';
-import { useState } from 'react';
-import ColorToken from './ColorToken';
-import ColorMenu from './ColorMenu';
-
-const ColorGroup = ({ ColorGroup }) => {
-
+const ColorGroup = () => {
+  const colorGroup = data;
   const [open, setOpen] = useState(false);
-
   const handleMenu = (option) => {
-    console.log('handleMenu: ', option)
+    console.log("handleMenu: ", option);
     setOpen(!open);
   };
 
   return (
-    <div className="color-group-color-group">
-      <div className="color-group-container">
-        <h3 className="color-group-text">{ColorGroup.name}</h3>
-        <svg viewBox="0 0 1024 1024" className="color-group-icon"
-          onClick={handleMenu}
-        >
-          <path d="M512 426q34 0 60 26t26 60-26 60-60 26-60-26-26-60 26-60 60-26zM768 426q34 0 60 26t26 60-26 60-60 26-60-26-26-60 26-60 60-26zM256 426q34 0 60 26t26 60-26 60-60 26-60-26-26-60 26-60 60-26z"></path>
-        </svg>
-        {open && <ColorMenu handleMenu={handleMenu} />}
-      </div>
-      <div className="color-group-container1">
-        {
-          ColorGroup.tokens.map(({ name, color }, index) => (
-            <ColorToken name={name} color={color} />
-          ))
-        }
-        <div className="color-group-container2">
-          <svg viewBox="0 0 1024 1024" className="color-group-icon2">
-            <path d="M213.333 554.667h256v256c0 23.552 19.115 42.667 42.667 42.667s42.667-19.115 42.667-42.667v-256h256c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-256v-256c0-23.552-19.115-42.667-42.667-42.667s-42.667 19.115-42.667 42.667v256h-256c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
-          </svg>
+    <>
+      {colorGroup.categories.map((category, idx) => (
+        <div id={idx} className="color-group-color-group">
+          <div className="color-group-container">
+            <h3 className="color-group-text">{category.name}</h3>
+            <svg
+              viewBox="0 0 1024 1024"
+              className="color-group-icon"
+              onClick={handleMenu}
+            >
+              <path d="M512 426q34 0 60 26t26 60-26 60-60 26-60-26-26-60 26-60 60-26zM768 426q34 0 60 26t26 60-26 60-60 26-60-26-26-60 26-60 60-26zM256 426q34 0 60 26t26 60-26 60-60 26-60-26-26-60 26-60 60-26z"></path>
+            </svg>
+            {open && <ColorMenu handleMenu={handleMenu} />}
+          </div>
+          <div className="color-group-container1">
+            {category.tokens.map((token) => (
+              <ColorToken name={token.name} color={token.color.hex} />
+            ))}
+            <div className="color-group-container2">
+              <svg viewBox="0 0 1024 1024" className="color-group-icon2">
+                <path d="M213.333 554.667h256v256c0 23.552 19.115 42.667 42.667 42.667s42.667-19.115 42.667-42.667v-256h256c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-256v-256c0-23.552-19.115-42.667-42.667-42.667s-42.667 19.115-42.667 42.667v256h-256c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+              </svg>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
