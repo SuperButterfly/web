767e265d640a9c427a47ff6aad4850c375858e57
Pauli <94504453+Sil-Pauli@users.noreply.github.com>
2023-06-28 14:38:38 -0300

    30134 Crear este front

---
+++ src/Pages/dashboard/Editor/codeScreen/visual-front-30134/NuevoFront.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  './NuevoFront.module.css';
+import { divide } from 'lodash';
+
+const NuevoFront =()=>{
+    return (
+        <div className='general'>
+            <div className='rectangulo'></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+         
+        </div>
+      );
+    };
+export default NuevoFront;
