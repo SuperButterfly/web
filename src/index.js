844e1edac31d50ed4cefdf7c61b464293778c2b0
Roger Miño <roger.rm03@gmail.com>
2023-06-28 15:28:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://api-web.aythen.com/api';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
