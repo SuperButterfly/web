1a81b0d61292e0bd6d79eb722299cf9d15850dea
Martín Araujo <mnaheavy@gmail.com>
2023-06-27 22:34:33 -0300

    pressets-css

--- src/Pages/dashboard/Editor/pressets/ColorGroup.js
+++ src/Pages/dashboard/Editor/pressets/ColorGroup.js
@@ -31,11 +31,19 @@ const ColorGroup = () => {
             {category.tokens.map((token) => (
               <ColorToken name={token.name} color={token.color.hex} />
             ))}
-            <div className="color-group-container2">
-              <svg viewBox="0 0 1024 1024" className="color-group-icon2">
-                <path d="M213.333 554.667h256v256c0 23.552 19.115 42.667 42.667 42.667s42.667-19.115 42.667-42.667v-256h256c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-256v-256c0-23.552-19.115-42.667-42.667-42.667s-42.667 19.115-42.667 42.667v256h-256c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
-              </svg>
-            </div>
+            <button class="pt-bton">
+              <div class="pt-iconx ">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="#363636"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 3a.5.5 0 00-1 0v3H3a.5.5 0 000 1h3v3a.5.5 0 001 0V7h3a.5.5 0 000-1H7V3z"></path>
+                </svg>
+              </div>
+            </button>
           </div>
         </div>
       ))}
