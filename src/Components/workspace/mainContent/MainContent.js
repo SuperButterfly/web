5e5991c646ef8021ae68cc21ce07b5e8d392ec2d
Mariana <mariana.stocco@outlook.com>
2023-06-30 19:45:59 -0300

    30/06-Buttons menu in styling process

--- src/Components/workspace/mainContent/MainContent.js
+++ src/Components/workspace/mainContent/MainContent.js
@@ -69,8 +69,8 @@ const MainContent = () => {
     const valueResult = viewerValue
       ? "viewer"
       : editorValue
-      ? "editor"
-      : "none";
+        ? "editor"
+        : "none";
 
     if (data) {
       console.log("data", data);
@@ -302,7 +302,7 @@ const MainContent = () => {
               /*(!islimited) ? 'main-content-button-notactive' :*/ "main-content-button"
             }
             onClick={() => handleCreateProject()}
-            //disabled={!islimited}
+          //disabled={!islimited}
           >
             + Create project
           </button>
@@ -532,123 +532,123 @@ const MainContent = () => {
         </div>
       </div>
       <div className="main-content-container04">
-          <div className="main-content-container05">
-            <input
-              type="text"
-              name="search"
-              onChange={handleSearchChange}
-              /*  id="input-search" */
-              className="main-content-text1"
-              placeholder="Search"
-            />
-            <button
-              onClick={() => handleSearch()}
-              className="main-content-button1"
-            >
-              <svg viewBox="0 0 1024 1024" className="main-content-icon">
-                <path d="M406 598q80 0 136-56t56-136-56-136-136-56-136 56-56 136 56 136 136 56zM662 598l212 212-64 64-212-212v-34l-12-12q-76 66-180 66-116 0-197-80t-81-196 81-197 197-81 196 81 80 197q0 42-20 95t-46 85l12 12h34z"></path>
-              </svg>
-            </button>
-          </div>
-
-          <div className="main-content-thq-dropdown">
-            <div className="main-content-dropdown-toggle">
-              <span className="main-content-text2">Order by</span>
-              <div className="main-content-dropdown-arrow">
-                <svg viewBox="0 0 1024 1024" className="main-content-drop-icon">
-                  <path d="M426 726v-428l214 214z"></path>
-                </svg>
-              </div>
-            </div>
-            <ul className="main-content-dropdown-list">
-              <li className="main-content-dropdown list-item">
-                <div className="main-content-dropdown-toggle1">
-                  <span className="main-content-created">created</span>
-                </div>
-              </li>
-              <li className="main-content-dropdown1 list-item">
-                <div className="main-content-dropdown-toggle2">
-                  <span className="main-content-date">date</span>
-                </div>
-              </li>
-              <li className="main-content-dropdown2 list-item">
-                <div className="main-content-dropdown-toggle3">
-                  <span className="main-content-a-z">A - Z</span>
-                </div>
-              </li>
-            </ul>
-          </div>
+        <div className="main-content-container05">
+          <input
+            type="text"
+            name="search"
+            onChange={handleSearchChange}
+            /*  id="input-search" */
+            className="main-content-text1"
+            placeholder="Search"
+          />
+          <button
+            onClick={() => handleSearch()}
+            className="main-content-button1"
+          >
+            <svg viewBox="0 0 1024 1024" className="main-content-icon">
+              <path d="M406 598q80 0 136-56t56-136-56-136-136-56-136 56-56 136 56 136 136 56zM662 598l212 212-64 64-212-212v-34l-12-12q-76 66-180 66-116 0-197-80t-81-196 81-197 197-81 196 81 80 197q0 42-20 95t-46 85l12 12h34z"></path>
+            </svg>
+          </button>
         </div>
+
+        <div className="main-content-thq-dropdown">
+          <div className="main-content-dropdown-toggle">
+            <span className="main-content-text2">Order by</span>
+            <div className="main-content-dropdown-arrow">
+              <svg viewBox="0 0 1024 1024" className="main-content-drop-icon">
+                <path d="M426 726v-428l214 214z"></path>
+              </svg>
+            </div>
+          </div>
+          <ul className="main-content-dropdown-list">
+            <li className="main-content-dropdown list-item">
+              <div className="main-content-dropdown-toggle1">
+                <span className="main-content-created">created</span>
+              </div>
+            </li>
+            <li className="main-content-dropdown1 list-item">
+              <div className="main-content-dropdown-toggle2">
+                <span className="main-content-date">date</span>
+              </div>
+            </li>
+            <li className="main-content-dropdown2 list-item">
+              <div className="main-content-dropdown-toggle3">
+                <span className="main-content-a-z">A - Z</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <ul className="main-content-ul list">
         <div>{createProjectWindow && <Mockup />}</div>
-       
+
         {workspace.projects && workspace.projects.length > 0
           ? (filteredSearch.length == 0
-              ? workspace.projects
-              : filteredSearch
-            ).map((project, idx) => (
-              <li className="list-item01" key={idx}>
-                <div className="main-content-workspace">
-                  <img
-                    alt="project"
-                    src={`${urlbase}/AythenLogo.png`}
-                    className="main-content-icon4"
-                  />
-                  <div
-                    className="main-content-container06"
-                    onClick={() => handleProject(project.id)}
-                  >
-                    <span className="main-content-text3">{project.name}</span>
-                    <div className="main-content-timestamps">
-                      <span className="main-content-text4">
-                        created:{" "}
-                        {`${project.createdAt.slice(
-                          0,
-                          10
-                        )} - updated: ${project.updatedAt.slice(0, 10)}`}
-                      </span>
-                    </div>
-                  </div>
-                  <div
-                    className="main-content-menu"
-                    onClick={() => handleMenu(idx)}
-                  >
-                    <span>. . .</span>
-                  </div>
-
-                  <div
-                    className="main-content-container07"
-                    id={idx}
-                    style={{
-                      visibility: idx === idVisible ? "visible" : "hidden",
-                    }}
-                  >
-                    <span className="main-content-clone">Clone</span>
-                    <div className="main-content-container08">
-                      <span className="main-content-delete">Delete</span>
-                      <div className="main-content-container09">
-                        <span>del</span>
-                        <svg
-                          viewBox="0 0 1024 1024"
-                          className="main-content-icon5"
-                        >
-                          <path d="M896 213.333c11.776 0 22.4 4.736 30.165 12.501s12.501 18.389 12.501 30.165v512c0 11.776-4.736 22.4-12.501 30.165s-18.389 12.501-30.165 12.501h-535.296l-261.333-298.667 261.333-298.667zM896 128h-554.667c-12.8 0-24.235 5.632-32.128 14.549l-298.667 341.333c-14.208 16.213-13.909 40.192 0 56.192l298.667 341.333c8.448 9.643 20.224 14.549 32.128 14.592h554.667c35.328 0 67.413-14.379 90.496-37.504s37.504-55.168 37.504-90.496v-512c0-35.328-14.379-67.413-37.504-90.496s-55.168-37.504-90.496-37.504zM481.835 414.165l97.835 97.835-97.835 97.835c-16.683 16.683-16.683 43.691 0 60.331s43.691 16.683 60.331 0l97.835-97.835 97.835 97.835c16.683 16.683 43.691 16.683 60.331 0s16.683-43.691 0-60.331l-97.835-97.835 97.835-97.835c16.683-16.683 16.683-43.691 0-60.331s-43.691-16.683-60.331 0l-97.835 97.835-97.835-97.835c-16.683-16.683-43.691-16.683-60.331 0s-16.683 43.691 0 60.331z"></path>
-                        </svg>
-                      </div>
-                    </div>
-                    <div className="main-content-hr"></div>
-                    <span
-                      className="main-content-settings"
-                      id="1"
-                      onClick={() => handleMenuClick(project.id)}
-                    >
-                      Project settings
+            ? workspace.projects
+            : filteredSearch
+          ).map((project, idx) => (
+            <li className="list-item01" key={idx}>
+              <div className="main-content-workspace">
+                <span className="main-content-icon4">
+                  {user && user.username
+                    ? user.username.slice(0, 1).toUpperCase()
+                    : "U"}
+                </span>
+                <div
+                  className="main-content-container06"
+                  onClick={() => handleProject(project.id)}
+                >
+                  <span className="main-content-text3">{project.name}</span>
+                  <div className="main-content-timestamps">
+                    <span className="main-content-text4">
+                      created:{" "}
+                      {`${project.createdAt.slice(
+                        0,
+                        10
+                      )} - updated: ${project.updatedAt.slice(0, 10)}`}
                     </span>
                   </div>
                 </div>
-              </li>
-            ))
+                <div
+                  className="main-content-menu"
+                  onClick={() => handleMenu(idx)}
+                >
+                  <span>. . .</span>
+                </div>
+
+                <div
+                  className="main-content-container07"
+                  id={idx}
+                  style={{
+                    visibility: idx === idVisible ? "visible" : "hidden",
+                  }}
+                >
+                  <span className="main-content-clone">Clone</span>
+                  <div className="main-content-container08">
+                    <span className="main-content-delete">Delete</span>
+                    <div className="main-content-container09">
+                      <span>del</span>
+                      <svg
+                        viewBox="0 0 1024 1024"
+                        className="main-content-icon5"
+                      >
+                        <path d="M896 213.333c11.776 0 22.4 4.736 30.165 12.501s12.501 18.389 12.501 30.165v512c0 11.776-4.736 22.4-12.501 30.165s-18.389 12.501-30.165 12.501h-535.296l-261.333-298.667 261.333-298.667zM896 128h-554.667c-12.8 0-24.235 5.632-32.128 14.549l-298.667 341.333c-14.208 16.213-13.909 40.192 0 56.192l298.667 341.333c8.448 9.643 20.224 14.549 32.128 14.592h554.667c35.328 0 67.413-14.379 90.496-37.504s37.504-55.168 37.504-90.496v-512c0-35.328-14.379-67.413-37.504-90.496s-55.168-37.504-90.496-37.504zM481.835 414.165l97.835 97.835-97.835 97.835c-16.683 16.683-16.683 43.691 0 60.331s43.691 16.683 60.331 0l97.835-97.835 97.835 97.835c16.683 16.683 43.691 16.683 60.331 0s16.683-43.691 0-60.331l-97.835-97.835 97.835-97.835c16.683-16.683 16.683-43.691 0-60.331s-43.691-16.683-60.331 0l-97.835 97.835-97.835-97.835c-16.683-16.683-43.691-16.683-60.331 0s-16.683 43.691 0 60.331z"></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="main-content-hr"></div>
+                  <span
+                    className="main-content-settings"
+                    id="1"
+                    onClick={() => handleMenuClick(project.id)}
+                  >
+                    Project settings
+                  </span>
+                </div>
+              </div>
+            </li>
+          ))
           : null}
       </ul>
     </div>
