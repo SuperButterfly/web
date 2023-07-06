6840db26eccd4f41bfe3bd2e9a7e2be4b0b2daf4
Pauli <94504453+Sil-Pauli@users.noreply.github.com>
2023-07-06 17:03:46 -0300

    feat/Style: que funcione el diagrama

--- src/Pages/dashboard/Editor/codeScreen/visual-front-30134/Diagram.js
+++ src/Pages/dashboard/Editor/codeScreen/visual-front-30134/Diagram.js
@@ -6,53 +6,112 @@ const Diagram = () => {
   const [shapes, setShapes] = useState([]);
 
   const handleShapeClick = (shapeType) => {
-    // Lógica para agregar una nueva forma al diagrama
-    // Puedes implementar esta función según tus necesidades
-    // Puede implicar agregar un nuevo objeto a la matriz "shapes" con información sobre la forma (tipo, posición, etc.)
+    const position = { x: 100, y: 100 }; 
+    const newShape = { type: shapeType, position };
+    setShapes([...shapes, newShape]);
+  };
+  
+  const handleShapeDrag = (event, index) => {
+    const newPosition = {
+        x: event.clientX,
+        y: event.clientY,
+    };
+    const updatedShapes = [...shapes];
+    updatedShapes[index].position = newPosition;
+    setShapes(updatedShapes);
   };
 
   return (
-        <div className={styles.diagramContainer}>
-            <div className={styles.workspace}>
-                <div className={styles.containerTitle}>
-                    <svg className={styles.iconSetting}></svg>
-                    <h2 className={styles.workspaceTitle}>Setting</h2>
-                </div>
-                <div className={styles.workspaceIcons}>
-                    <div className={styles.shape} onClick={() => handleShapeClick('aythen')}>
-                        <img className={styles.logoAythen} src={logoAythen}></img> 
-                    </div>
-                    <h5 className={styles.iconTitle}>Aythen</h5>
-                    <div className={styles.shape} onClick={() => handleShapeClick('slack')}>
-                        <svg className={styles.iconSlack}></svg>  
-                    </div>
-                    <h5 className={styles.iconTitle}>Slack</h5>
-                    <div className={styles.shape} onClick={() => handleShapeClick('trello')}>
-                        <svg className={styles.iconTrello}></svg>  
-                    </div>
-                    <h5 className={styles.iconTitle}>Trello</h5>
-                    <div className={styles.shape} onClick={() => handleShapeClick('gmail')}>
-                        <svg className={styles.iconGmail}></svg>  
-                    </div>
-                    <h5 className={styles.iconTitle}>Gmail</h5>
-                    <div className={styles.shape} onClick={() => handleShapeClick('exel')}>
-                        <svg className={styles.iconExcel}></svg>  
-                    </div>
-                    <h5 className={styles.iconTitle}>Excel</h5>
-                    <div className={styles.shape} onClick={() => handleShapeClick('line')}>
-                        <svg className={styles.iconLine}></svg>  
-                    </div>
-                    <h5 className={styles.iconTitle}>Line</h5>
-                </div>
+    <div className={styles.diagramContainer}>
+        <div className={styles.workspace}>
+            <div className={styles.containerTitle}>
+                <svg className={styles.iconSetting}></svg>
+                <h2 className={styles.workspaceTitle}>Setting</h2>
             </div>
-            <div className={styles.diagram}>
-                {shapes.map((shape, index) => (
-                <div key={index} className={styles.diagramShape}>
+            <div className={styles.workspaceIcons}>
+                <div className={styles.shape} onClick={() => handleShapeClick('aythen')}>
+                    <img className={styles.logoAythen} src={logoAythen} alt="Aythen"></img> 
                 </div>
-                ))}
+                <h5 className={styles.iconTitle}>Aythen</h5>
+                <div className={styles.shape} onClick={() => handleShapeClick('slack')}>
+                    <svg className={styles.iconSlack} alt="Slack"></svg>  
+                </div>
+                <h5 className={styles.iconTitle}>Slack</h5>
+                <div className={styles.shape} onClick={() => handleShapeClick('trello')}>
+                    <svg className={styles.iconTrello} alt="Trello"></svg>  
+                </div>
+                <h5 className={styles.iconTitle}>Trello</h5>
+                <div className={styles.shape} onClick={() => handleShapeClick('gmail')}>
+                    <svg className={styles.iconGmail} alt="Gmail"></svg>  
+                </div>
+                <h5 className={styles.iconTitle}>Gmail</h5>
+                <div className={styles.shape} onClick={() => handleShapeClick('excel')}>
+                    <svg className={styles.iconExcel} alt="Excel"></svg>  
+                </div>
+                <h5 className={styles.iconTitle}>Excel</h5>
             </div>
         </div>
-    );
+        <div className={styles.diagram}>
+            <div className={styles.diagramContent}>
+                {shapes.map((shape, index) => (
+                    <React.Fragment key={index}>
+                    <div
+                    className={styles.diagramShape}
+                    style={{ left: shape.position.x, top: shape.position.y }}
+                    draggable
+                    onDragStart={(event) => event.preventDefault()}
+                    onDrag={(event) => handleShapeDrag(event, index)}>
+                
+                {shape.type === 'aythen' && (
+                                
+                                    <div className={styles.infoRectangle}>
+                                    <img className={styles.logoAythen} src={logoAythen} alt="Aythen" />
+                                        <p className={styles.infoText}>Info</p>
+                                    </div>
+                                
+                            )}
+                {shape.type === 'slack' &&(
+                    
+                                    <div className={styles.infoRectangle}>
+                                    <svg className={styles.iconSlack} alt="Slack"></svg>
+                                        <p className={styles.infoText}>Info</p>
+                                    </div>
+                                
+                )}
+                 {shape.type === 'trello' &&(
+                    
+                                    <div className={styles.infoRectangle}>
+                                    <svg className={styles.iconTrello} alt="Trello"></svg>
+                                        <p className={styles.infoText}>Info</p>
+                                    </div>
+                                
+                )}
+                 {shape.type === 'gmail' &&(
+                    
+                                    <div className={styles.infoRectangle}>
+                                    <svg className={styles.iconGmail} alt="Gmail"></svg>
+                                        <p className={styles.infoText}>Info</p>
+                                    </div>
+                                
+                )}
+                {shape.type === 'excel' &&(
+                    
+                                    <div className={styles.infoRectangle}>
+                                    <svg className={styles.iconExcel} alt="Excel"></svg>
+                                        <p className={styles.infoText}>Info</p>
+                                    </div>
+                              
+                )}
+                </div>
+                {index < shapes.length + 1 && 
+                <hr className={styles.dottedLine} />}
+               
+                </React.Fragment>
+            ))}
+        </div>
+        </div>
+    </div>
+  );
 };
 
 export default Diagram;
