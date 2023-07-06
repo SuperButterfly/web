01d840f75a662e6786009ea9d74555b61fc94e42
pablo-salituri <pablo_salituri@live.com.ar>
2023-07-06 16:30:11 -0300

    Input with searchbar

--- src/Pages/dashboard/Editor/dataTablesScreen/Main/DropdownPopup/UnselectedLabels/UnselectedLabels.jsx
+++ src/Pages/dashboard/Editor/dataTablesScreen/Main/DropdownPopup/UnselectedLabels/UnselectedLabels.jsx
@@ -1,10 +1,10 @@
 import styles from './UnselectedLabels.module.css'
 
-export default function unselectedLabels({database, handleSelectLabel}) {
+export default function unselectedLabels({database, handleSelectLabel, input}) {
     return(
         <div className={styles.unselectedContainer}>
             {database.map((label, index) => {
-                if (label.selected === false) {
+                if (label.selected === false && label.value.includes(input)) {
                     return(
                         <button onClick={() => handleSelectLabel(index)} key={index} className={styles.unselectedLabel}>
                             {label.value}
