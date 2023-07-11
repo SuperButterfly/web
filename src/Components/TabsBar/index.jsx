0240f3fab3c0bbbf3a949ab924f0aaff09e03b1f
DianaQuintana <dcqy.95@gmail.com>
2023-07-11 00:28:27 -0500

    feat/drag-and-drog-in-screen-code

--- src/Components/TabsBar/index.jsx
+++ src/Components/TabsBar/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import TabComponent from './TabComponent'
 import styled from './tabsBar.module.css'
 
-const TabsBar = ({ files, onEdit, onClose, screenFile }) => {
+const TabsBar = ({ files, onEdit, onClose, screenFile, onDrag }) => {
   const [onScreen, setOnScreen] = useState(screenFile)
 
   const onCloseTab = (target) => {
@@ -10,10 +10,27 @@ const TabsBar = ({ files, onEdit, onClose, screenFile }) => {
   }
 
   const onEditTab = (target) => {
-    onEdit(files.find(e => e.file === target))
+    onEdit(files.find((e) => e.file === target))
   }
 
-  const tranformToTabComponent = (e) => {
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData('text/plain', index)
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault()
+  }
+
+  const handleDrop = (e, index) => {
+    const draggedIndex = e.dataTransfer.getData('text/plain')
+    const draggedItem = files[draggedIndex]
+    const newItems = [...files]
+    newItems.splice(draggedIndex, 1)
+    newItems.splice(index, 0, draggedItem)
+    onDrag(newItems)
+  }
+
+  const tranformToTabComponent = (e, index) => {
     return (
       <TabComponent
         file={e.file}
@@ -21,25 +38,27 @@ const TabsBar = ({ files, onEdit, onClose, screenFile }) => {
         onClose={onCloseTab}
         onEdit={onEditTab}
         screenFile={onScreen}
-      />)
+        key={index}
+        index={index}
+        handleDragStart={handleDragStart}
+        onDragOver={handleDragOver}
+        handleDrop={handleDrop}
+        className="item"
+      />
+    )
   }
 
   const [allFiles, setAllFiles] = useState(files.map(tranformToTabComponent))
 
   useEffect(() => {
     setAllFiles(files.map(tranformToTabComponent))
-  }
-  , [files, onScreen])
+  }, [files, onScreen])
 
-  useEffect(
-    () => { setOnScreen(screenFile) }, [screenFile]
-  )
+  useEffect(() => {
+    setOnScreen(screenFile)
+  }, [screenFile])
 
-  return (
-    <div className={styled.tabsBar}>
-      {allFiles}
-    </div>
-  )
+  return <div className={styled.tabsBar}>{allFiles}</div>
 }
 
 export default TabsBar
