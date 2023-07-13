415e5ed3cb044632db9e78cd7ac2da9a86920239
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-07-13 14:35:54 -0300

    Add redes settings

---
+++ src/Components/workspace/RedesSettings/RedesSettings.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import style from './RedesSettings.module.css'
+const RedesSettings = () => {
+  return (
+    <div className={style.container}>
+      <h4>Configuracion / Enlaces de redes sociales</h4>
+      <div className={style.container_flex_column}>
+        <div className={style.container_flex_row}>
+          <label htmlFor="facebook">Facebook</label>
+          <Link>Conectar o crear una pagina</Link>
+        </div>
+        <input
+          type="text"
+          name="facebook"
+          id="facebook"
+          placeholder="https//www.facebook.com/xxxx"
+        />
+      </div>
+      <div className={style.container_flex_column}>
+        <div className={style.container_flex_row}>
+          <label htmlFor="twitter">Twitter</label>
+          <Link>Conectar a tu cuenta</Link>
+        </div>
+        <input
+          type="text"
+          name="twitter"
+          id="twitter"
+          placeholder="https//www.twitter.com/xxxx"
+        />
+      </div>
+      <div className={style.container_flex_column}>
+        <div className={style.container_flex_row}>
+          <label htmlFor="instagram">Instagram</label>
+          <Link>Conectarse a una cuenta comercial</Link>
+        </div>
+        <input
+          type="text"
+          name="instagram"
+          id="instagram"
+          placeholder="https//www.instagram.com/xxxx"
+        />
+      </div>
+      <div className={style.container_flex_column}>
+        <div className={style.container_flex_row}>
+          <label htmlFor="youtube">Youtube</label>
+        </div>
+        <input
+          type="text"
+          name="youtube"
+          id="youtube"
+          placeholder="https//www.youtube.com/xxxx"
+        />
+      </div>
+      <div className={style.container_flex_column}>
+        <div className={style.container_flex_row}>
+          <label htmlFor="linkedin">Linkedin</label>
+        </div>
+        <input
+          type="text"
+          name="linkedin"
+          id="linkedin"
+          placeholder="https//www.linkedin.com/xxxx"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default RedesSettings
