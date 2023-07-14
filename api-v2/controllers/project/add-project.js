3dc758a10d7e833b123ac8a69b022350cb40329f
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-14 16:11:18 -0300

    Controladores de Page

--- api-v2/controllers/project/add-project.js
+++ api-v2/controllers/project/add-project.js
@@ -1,3 +1,15 @@
-const addProject = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { addProject }
+const addProject = async (req, res, next) => {
+  const body = req.body
+
+  const newProject = await models.ProjectModel.create(body)
+
+  if (!newProject) throw new ClientError('Erro al crear el project', 400)
+
+  response(res, 200, newProject)
+}
+
+module.exports = { addProject: catchedAsync(addProject) }
