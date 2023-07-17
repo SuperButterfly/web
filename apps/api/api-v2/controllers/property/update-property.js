3b2b50abfca3af7648887798dd8728353f4e1f35
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-17 12:26:21 -0300

    #50409 PROPERTY
    fix

--- apps/api/api-v2/controllers/property/update-property.js
+++ apps/api/api-v2/controllers/property/update-property.js
@@ -5,10 +5,8 @@ const { ClientError } = require('../../utils/err/errors')
 const updateProperty = async (req, res, next) => {
   const { body } = req.body
   const { id } = req.params
-  const property = await models.PropertyModel.findOne({ where: { id } })
-  if (!property) {
-    throw new ClientError('Property Not Found', 404)
-  }
+  const property = await models.PropertyModel.findByPk(id)
+  if (!property) throw new ClientError('Property Not Found', 404)
   await property.update(body)
   response(res, 201, property)
 }
