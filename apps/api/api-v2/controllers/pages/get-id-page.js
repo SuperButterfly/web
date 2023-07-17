40927d82f9022e5f117341793e2767ccd8d66edb
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 09:03:31 -0300

    Estandarizacion de get id page

--- apps/api/api-v2/controllers/pages/get-id-page.js
+++ apps/api/api-v2/controllers/pages/get-id-page.js
@@ -1,3 +1,15 @@
-const getIdPage = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { getIdPage }
+const getIdPage = async (req, res, next) => {
+  const { id } = req.params
+
+  const page = await models.PageModel.findByPk(id)
+
+  if (!page) throw new ClientError('Failed to find preset by ID', 404)
+
+  response(res, 200, page)
+}
+
+module.exports = { getIdPage: catchedAsync(getIdPage) }
