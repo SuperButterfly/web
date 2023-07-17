ef511b0b698cb564044c1c724b42c25a64547fea
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 09:04:08 -0300

    Estandarizacion de delete page

--- apps/api/api-v2/controllers/pages/delete-page.js
+++ apps/api/api-v2/controllers/pages/delete-page.js
@@ -1,3 +1,19 @@
-const deletePage = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { deletePage }
+const deletePage = async (req, res, next) => {
+  const { id } = req.params
+
+  const page = await models.PageModel.findOne({ where: { id: id } })
+
+  if (!page) throw new ClientError('Page not found', 404)
+
+  await page.update({ isDeleted: true })
+
+  const pageDeleted = await models.PageModel.findOne({ where: { id: id } })
+
+  response(res, 200, pageDeleted)
+}
+
+module.exports = { deletePage: catchedAsync(deletePage) }
