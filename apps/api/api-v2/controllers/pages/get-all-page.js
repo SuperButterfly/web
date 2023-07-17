0c5a502dd0a295f5216ea9ce992e9532817cd954
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 09:03:53 -0300

    Estandarizacion de get all page

--- apps/api/api-v2/controllers/pages/get-all-page.js
+++ apps/api/api-v2/controllers/pages/get-all-page.js
@@ -1,3 +1,13 @@
-const getAllPage = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { getAllPage }
+const getAllPage = async (req, res, next) => {
+  const allPages = await models.PageModel.findAll()
+
+  if (!allPages) throw new ClientError('Error fetching the pages', 404)
+
+  response(res, 200, allPages)
+}
+
+module.exports = { getAllPage: catchedAsync(getAllPage) }
