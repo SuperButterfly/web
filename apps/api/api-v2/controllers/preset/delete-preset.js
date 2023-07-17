b4a731e7d07888084b465e473aeb62611d6f4d7d
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 09:00:48 -0300

    Estandarizacion de delete preset

--- apps/api/api-v2/controllers/preset/delete-preset.js
+++ apps/api/api-v2/controllers/preset/delete-preset.js
@@ -1,3 +1,19 @@
-const deletePreset = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { deletePreset }
+const deletePreset = async (req, res, next) => {
+  const { id } = req.params
+
+  const preset = await models.PresetModel.findOne({ where: { id: id } })
+
+  if (!preset) throw new ClientError('Preset not found', 404)
+
+  await preset.update({ isDeleted: true })
+
+  const presetDeleted = await models.PresetModel.findOne({ where: { id: id } })
+
+  response(res, 200, presetDeleted)
+}
+
+module.exports = { deletePreset: catchedAsync(deletePreset) }
