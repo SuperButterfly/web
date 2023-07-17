3a61e43763fb88648b9c49ef434d197c937f1765
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 08:59:39 -0300

    Estandarizacion de patch preset

--- apps/api/api-v2/controllers/preset/patch-preset.js
+++ apps/api/api-v2/controllers/preset/patch-preset.js
@@ -1,3 +1,20 @@
-const patchPreset = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { patchPreset }
+const patchPreset = async (req, res, next) => {
+  const { id } = req.params
+  const body = req.body
+
+  const preset = await models.PresetModel.findOne({ where: { id: id } })
+
+  if (!preset) throw new ClientError('Preset not found', 404)
+
+  await preset.update(body)
+
+  const presetUpdated = await models.PresetModel.findOne({ where: { id: id } })
+
+  response(res, 200, presetUpdated)
+}
+
+module.exports = { patchPreset: catchedAsync(patchPreset) }
