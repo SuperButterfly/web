7356b41b4c9137603f51ad436343adf1fa6bd932
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 09:04:27 -0300

    Estandarizacion de add page

--- apps/api/api-v2/controllers/pages/add-page.js
+++ apps/api/api-v2/controllers/pages/add-page.js
@@ -1,3 +1,15 @@
-const addPage = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { addPage }
+const addPage = async (req, res, next) => {
+  const body = req.body
+
+  const newPage = await models.PageModel.create(body)
+
+  if (!newPage) throw new ClientError('Error to create page', 400)
+
+  response(res, 200, newPage)
+}
+
+module.exports = { addPage: catchedAsync(addPage) }
