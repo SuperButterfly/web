900316872eaabab1c371d33bcdcb7db3bb4687d2
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 10:25:14 -0300

    Correccion y especificacion en los modelos

--- apps/api/api-v2/database/models/project.js
+++ apps/api/api-v2/database/models/project.js
@@ -22,7 +22,7 @@ module.exports = (sequelize) =>
       allowNull: false
     },
     pre_sets: {
-      type: DataTypes.JSONB,
+      type: DataTypes.ARRAY(DataTypes.JSONB),
       allowNull: false
     },
     isDeleted: {
