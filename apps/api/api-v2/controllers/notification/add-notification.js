860ca3a86ebe69368f523050a4578c0f8c60f734
FedericoA15 <federicoalmeida15@gmail.com>
2023-07-17 13:12:21 -0300

    create add notification

---
+++ apps/api/api-v2/controllers/notification/add-notification.js
@@ -0,0 +1,10 @@
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+
+const addNotification = async (req, res, next) => {
+  const { body } = req.body
+  const notification = await models.NotificationModel.create(body)
+  response(res, 200, notification)
+}
+
+module.exports = { addNotification: catchedAsync(addNotification) }
