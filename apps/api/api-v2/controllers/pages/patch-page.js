5e80e40111345063ef577d022f2fcd7f128a2efb
AgustinChavero <agustin1996orellano@gmail.com>
2023-07-17 09:03:08 -0300

    Estandarizacion de patch page

--- apps/api/api-v2/controllers/pages/patch-page.js
+++ apps/api/api-v2/controllers/pages/patch-page.js
@@ -1,3 +1,20 @@
-const patchPage = async (req, res) => {}
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
 
-module.exports = { patchPage }
+const patchPage = async (req, res, next) => {
+  const { id } = req.params
+  const body = req.body
+
+  const page = await models.PageModel.findOne({ where: { id } })
+
+  if (!page) throw new ClientError('Page not found', 404)
+
+  await page.update(body)
+
+  const pageUpdated = await models.PageModel.findOne({ where: { id } })
+
+  response(res, 200, pageUpdated)
+}
+
+module.exports = { patchPage: catchedAsync(patchPage) }
