4aa2589b1c6e445b72edcebee7cbacadbd45d4d1
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-17 12:26:30 -0300

    #50409 PROPERTY
    refactor

--- apps/api/api-v2/controllers/property/patch-property.js
+++ apps/api/api-v2/controllers/property/patch-property.js
@@ -5,16 +5,11 @@ const { ClientError } = require('../../utils/err/errors')
 const patchPreset = async (req, res, next) => {
   const { body } = req
   const { id } = req.params
-  const property = await models.PropertyModel.findOne({ where: { id } })
-
-  if (!property) {
-    throw new ClientError('Property Not Found', 400)
-  }
-
+  const property = await models.PropertyModel.findByPk(id)
+  if (!property) throw new ClientError('Property Not Found', 400)
   // Aplica solo las propiedades proporcionadas en el cuerpo de la solicitud (patch) en lugar de actualizar todo el objeto
   Object.assign(property, body)
   await property.save()
-
   response(res, 201, property)
 }
 
