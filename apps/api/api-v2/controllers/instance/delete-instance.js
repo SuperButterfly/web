04c9da20c66a9bfaf5d77cd92c299171b1de985a
FedericoA15 <federicoalmeida15@gmail.com>
2023-07-18 16:56:05 -0300

    crete delete intance

---
+++ apps/api/api-v2/controllers/instance/delete-instance.js
@@ -0,0 +1,19 @@
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
+
+const deleteInstance = async (req, res, next) => {
+  const { id } = req.params
+
+  const instance = await models.InstanceModel.findOne({ where: { id: id } })
+
+  if (!instance) throw new ClientError('Instance not found', 404)
+
+  await instance.update({ isDeleted: true })
+
+  await instance.save()
+
+  response(res, 200, instance)
+}
+
+module.exports = { deleteInstance: catchedAsync(deleteInstance) }
