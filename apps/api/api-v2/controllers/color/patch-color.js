89d9d8c2e297782ec98fa92b12537f924c5fc6b9
AgustinChavero <agustindanielchavero@gmail.com>
2023-07-18 09:26:11 -0300

    Crud de Color

--- apps/api/api-v2/controllers/color/patch-color.js
+++ apps/api/api-v2/controllers/color/patch-color.js
@@ -2,6 +2,19 @@ const { models } = require('../../database/connection/database')
 const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
-const patchColor = async (req, res, next) => {}
+const patchColor = async (req, res, next) => {
+    const { id } = req.params
+  const body = req.body
+
+  const color = await models.ColorModel.findOne({ where: { id } })
+
+  if (!color) throw new ClientError('Color not found', 404)
+
+  await color.update(body, { fields: Object.keys(body) })
+
+  await color.save()
+
+  response(res, 200, color)
+}
 
 module.exports = { patchColor: catchedAsync(patchColor) }
