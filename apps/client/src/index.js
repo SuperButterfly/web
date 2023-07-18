0e7b9f2cdd4f1fc6c47f617ced7cf92658050be9
Franco Gutierrez <franco_forex@hotmail.com>
2023-07-18 14:09:34 -0300

    fix: base url axios

--- apps/client/src/index.js
+++ apps/client/src/index.js
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://api-web2.aythen.com/api'
- axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.baseURL = 'https://api-web2.aythen.com/api'
+// axios.defaults.baseURL = "http://localhost:4000/api";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
