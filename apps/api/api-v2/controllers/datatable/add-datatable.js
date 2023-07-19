e3ee5ff93f1f0df5c8214f8eb1fb55ee155f5c92
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-19 12:24:35 -0300

    fix controller

--- apps/api/api-v2/controllers/datatable/add-datatable.js
+++ apps/api/api-v2/controllers/datatable/add-datatable.js
@@ -3,10 +3,10 @@ const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
 const addDatatable = async (req, res) => {
-  const { row } = req.body
-  
-  const newDatatable = await models.DatatableModel.create({ row })
-  
+  const { row, workSpaceId } = req.body
+
+  const newDatatable = await models.DatatableModel.create({ row, workSpaceId })
+
   if (!newDatatable) throw new ClientError('Create Datatable Failed', 400)
 
   return response(res, 200, newDatatable)
