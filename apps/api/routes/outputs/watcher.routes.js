f772a023d0d8844c3fc3603b2110672909841008
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-19 14:21:40 -0300

    metodos get para todos los watchers, watcher by id, watcher events y evento de su respectivo watcher para start on blockchain

---
+++ apps/api/routes/outputs/watcher.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+const watcherRouter = Router()
+const {
+  getAllWatchers,
+  getWatcherById,
+  getWatcherEvents,
+  getEventForWatcher
+  } = require('../../controllers/outputs/watcher.controllers.js')
+
+
+watcherRouter.get('/', getAllWatchers)
+watcherRouter.get('/:id', getWatcherById)
+watcherRouter.get('/:id/event', getWatcherEvents)
+watcherRouter.get('//:id/event/:eventId', getEventForWatcher)
+
+
+
+
+module.exports = watcherRouter
