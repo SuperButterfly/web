4212e76171ad2d2e611529a154db6d87dfb728df
httpfido <fidel.romano.2016@hotmail.com>
2023-07-19 18:16:33 -0300

    re-estructuracion componente Cards.js

--- apps/client/src/Components/workspace/mockup/Cards/Cards.js
+++ apps/client/src/Components/workspace/mockup/Cards/Cards.js
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import './Cards.css'
+import Modal from 'react-modal'
 
 export default function Cards() {
-  const [display, setDisplay] = useState({
-    plantilla1: false,
-    plantilla2: false,
-    plantilla3: false,
-    plantilla4: false,
-    plantilla5: false,
-    plantilla6: false,
-    plantilla7: false,
-    plantilla8: false,
-    plantilla9: false,
-    plantilla10: false,
-    plantilla11: false,
-    plantilla12: false
-  })
+  const [display, setDisplay] = useState({})
+  const [previewIsOpen, setPreviewIsOpen] = useState(false)
+  const [selectedTemplate, setSelectedTemplate] = useState(null)
+
+  const templates = [
+    { id: 'plantilla1', title: 'Start from scratch', type: 'Blank project' },
+    { id: 'plantilla2', title: 'SHOP Template', type: 'Template', img:"https://colorlib.com/wp/wp-content/uploads/sites/2/capitalshop-free-template.jpg.webp"},
+    { id: 'plantilla3', title: 'SEO Template', type: 'Template', img: "https://portermetrics.com/wp-content/uploads/2023/01/Porter-Metrics-Google-Analytics-4-GA4-Google-Data-Studio-Report-Template-›-Overview-1927x2048.png" },
+    { id: 'plantilla4', title: 'OFFICE Template', type: 'Template', img: "https://media.slidesgo.com/storage/84164/responsive-images/0-office-meeting___media_library_original_1600_900.jpg" },
+    { id: 'plantilla5', title: 'RRSS Template', type: 'Template', img: "https://edit.org/photos/img/blog/z8a-plantillas-para-redes-sociales-editor-online-gratis.jpg-840.jpg" },
+    { id: 'plantilla6', title: 'CRM template', type: 'Template', img: "https://clickup.com/blog/wp-content/uploads/2022/06/image3-4-1400x623.png" },
+    { id: 'plantilla7', title: 'CRM template', type: 'Template', img: "https://play.teleporthq.io/static/img/project-templates/togthr-app.gif" },
+    { id: 'plantilla8', title: 'Medica template', type: 'Template', img:"https://play.teleporthq.io/static/img/project-templates/togthr-app.gif" },
+    { id: 'plantilla9', title: 'Medica template', type: 'Template', img: "https://play.teleporthq.io/static/img/project-templates/togthr-app.gif" },
+    { id: 'plantilla10', title: 'Medica template', type: 'Template', img: "https://play.teleporthq.io/static/img/project-templates/togthr-app.gif" },
+    { id: 'plantilla11', title: 'Medica template', type: 'Template', img: "https://play.teleporthq.io/static/img/project-templates/togthr-app.gif" },
+    { id: 'plantilla12', title: 'Medica template', type: 'Template', img: "https://play.teleporthq.io/static/img/project-templates/togthr-app.gif" }
+  ]
 
   const changeDisplay = (key, value) => {
     setDisplay((prevState) => ({
@@ -24,292 +29,49 @@ export default function Cards() {
     }))
   }
 
+  const handlePreview = (templateId) => {
+    setSelectedTemplate(templateId)
+    setPreviewIsOpen(true)
+  }
+
+  const closePreview = () => {
+    setPreviewIsOpen(false)
+  }
+
   return (
     <>
-      <div className="plantilla1 template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla1', true)}
-          onMouseLeave={() => changeDisplay('plantilla1', false)}
-          className="blank-project"
-        >
-          <div className={display.plantilla1 ? 'card-overlay' : 'display'}>
-            <button className="btn-overlay start">Start Building</button>
-          </div>
-          <span className="blank-text">Blank</span>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">Start from scratch</span>
-            <div className="author-section">
-              <span className="template-type">Blank project</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla2', true)}
-          onMouseLeave={() => changeDisplay('plantilla2', false)}
-          className="plantilla2 img-container"
-        >
-          <div className={display.plantilla2 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
+      {templates.map((template) => (
+        <div key={template.id} className="template-card">
+          <div
+            onMouseEnter={() => changeDisplay(template.id, true)}
+            onMouseLeave={() => changeDisplay(template.id, false)}
+            className={`img-container ${template.id}`}
+          >
+            <div className={display[template.id] ? 'card-overlay' : 'display'}>
+              <div className="pt-stack-card">
+                <div className="btn-wrapper">
+                  <button
+                    className="btn-overlay preview"
+                    onClick={() => handlePreview(template.img)}
+                  >
+                    Quick Preview
+                  </button>
+                </div>
+                <button className="btn-overlay start">Start Building</button>
               </div>
-              <button className="btn-overlay start">Start Building</button>
             </div>
           </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">SHOP Template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla3', true)}
-          onMouseLeave={() => changeDisplay('plantilla3', false)}
-          className="plantilla3 img-container"
-        >
-          <div className={display.plantilla3 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
+          <div className="bottom-section">
+            <div className="left-side">
+              <span className="project-title">{template.title}</span>
+              <div className="author-section">
+                <span className="template-type">{template.type}</span>
               </div>
-              <button className="btn-overlay start">Start Building</button>
             </div>
           </div>
         </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">SEO Template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla4', true)}
-          onMouseLeave={() => changeDisplay('plantilla4', false)}
-          className="plantilla4 img-container"
-        >
-          <div className={display.plantilla4 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">OFFICE Template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla5', true)}
-          onMouseLeave={() => changeDisplay('plantilla5', false)}
-          className="plantilla5 img-container"
-        >
-          <div className={display.plantilla5 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">RRSS Template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla6', true)}
-          onMouseLeave={() => changeDisplay('plantilla6', false)}
-          className="plantilla6 img-container"
-        >
-          <div className={display.plantilla6 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">CRM template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla7', true)}
-          onMouseLeave={() => changeDisplay('plantilla7', false)}
-          className="plantilla7 img-container"
-        >
-          <div className={display.plantilla7 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">CRM template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla8', true)}
-          onMouseLeave={() => changeDisplay('plantilla8', false)}
-          className="plantilla8 img-container"
-        >
-          <div className={display.plantilla8 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">Medica template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla9', true)}
-          onMouseLeave={() => changeDisplay('plantilla9', false)}
-          className="plantilla9 img-container"
-        >
-          <div className={display.plantilla9 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">Medica template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla10', true)}
-          onMouseLeave={() => changeDisplay('plantilla10', false)}
-          className="plantilla10 img-container"
-        >
-          <div className={display.plantilla10 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">Medica template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla11', true)}
-          onMouseLeave={() => changeDisplay('plantilla11', false)}
-          className="plantilla11 img-container"
-        >
-          <div className={display.plantilla11 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">Medica template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="template-card">
-        <div
-          onMouseEnter={() => changeDisplay('plantilla12', true)}
-          onMouseLeave={() => changeDisplay('plantilla12', false)}
-          className="plantilla12 img-container"
-        >
-          <div className={display.plantilla12 ? 'card-overlay' : 'display'}>
-            <div className="pt-stack-card">
-              <div className="btn-wrapper">
-                <button className="btn-overlay preview">Quick Preview</button>
-              </div>
-              <button className="btn-overlay start">Start Building</button>
-            </div>
-          </div>
-        </div>
-        <div className="bottom-section">
-          <div className="left-side">
-            <span className="project-title">Medica template</span>
-            <div className="author-section">
-              <span className="template-type">Template </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
+      
     </>
   )
 }
