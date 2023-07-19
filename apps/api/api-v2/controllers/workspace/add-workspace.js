487b880128044459f1fe7c81c6c521b8e98ac6ea
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-19 12:24:41 -0300

    Update add-workspace.js

--- apps/api/api-v2/controllers/workspace/add-workspace.js
+++ apps/api/api-v2/controllers/workspace/add-workspace.js
@@ -4,7 +4,10 @@ const { catchedAsync, response } = require('../../utils/err')
 const addWorkSpace = async (req, res, next) => {
   const { name, description } = req.body
 
-  const workspace = await models.WorkSpaceModel.create({ name, description })
+  const workspace = await models.WorkSpaceModel.create({
+    name,
+    description
+  })
   response(res, 200, workspace)
 }
 
