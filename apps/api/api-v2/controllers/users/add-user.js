674c440e08f032f40290d219973a16a0c0f87a04
FedericoA15 <federicoalmeida15@gmail.com>
2023-07-19 14:43:01 -0300

    register

--- apps/api/api-v2/controllers/users/add-user.js
+++ apps/api/api-v2/controllers/users/add-user.js
@@ -1,11 +1,29 @@
 const { models } = require('../../database/connection/database')
+const bcrypt = require('bcrypt')
 const { catchedAsync, response } = require('../../utils/err')
-const { ClientError } = require('../../utils/err/errors')
+// const { ClientError } = require('../../utils/err/errors')
 
 const addUser = async (req, res, next) => {
-  const { username, email, password, plan, resourceslist, theme, billingDates } = req.body
+  const {
+    username,
+    email,
+    password,
+    plan,
+    resourceslist,
+    theme,
+    billingDates
+  } = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
 
-  const user = await models.UserModel.create({ username, email, password, plan, resourceslist, theme, billingDates })
+  const user = await models.UserModel.create({
+    username,
+    email,
+    password: hashedPassword,
+    plan,
+    resourceslist,
+    theme,
+    billingDates
+  })
   response(res, 200, user)
 }
 
