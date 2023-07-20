bb169350c723083c99123568f9dde2270762b132
FedericoA15 <federicoalmeida15@gmail.com>
2023-07-20 11:45:27 -0300

    new controller de project

--- apps/api/api-v2/controllers/project/add-project.js
+++ apps/api/api-v2/controllers/project/add-project.js
@@ -3,11 +3,27 @@ const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
 const addProject = async (req, res, next) => {
-  const { body } = req
+  const { name, workSpaceId, userToolId, role } = req.body
 
-  const newProject = await models.ProjectModel.create(body)
+  const workspace = await models.WorkSpaceModel.findByPk(workSpaceId)
+
+  if (!workspace) throw new ClientError('Error not found workspace', 400)
+
+  const user = await models.UserModel.findByPk(userToolId)
+
+  if (!user) {
+    throw new ClientError('Error not found user', 400)
+  }
+
+  const newProject = await models.ProjectModel.create(name)
 
   if (!newProject) throw new ClientError('Error to create project', 400)
+
+  await models.UserWorkSpaceModel.create({
+    userId: user.id,
+    projectId: newProject.id,
+    role: role
+  })
 
   response(res, 200, newProject)
 }
