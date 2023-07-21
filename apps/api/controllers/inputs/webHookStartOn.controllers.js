4322a3390c4debd3c72d13d0398b4dea5db4c93e
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:42:13 -0300

    webhook refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/inputs/webHookStartOn.controllers.js
+++ apps/api/controllers/inputs/webHookStartOn.controllers.js
@@ -1,67 +1,93 @@
+const startonApi = require('../../services/smartContractAxiosInstance')
 
-const startonApi = require("../../services/smartContractAxiosInstance")
-async function regenerateWebhookSigningSecret(id) {
-    const url = '/v3/webhook/signing-secret/regenerate';
-  
-    try {
-      const response = await startonApi.post(url);
-      console.log('Respuesta:', response.status, response.data);
-      return response.data; // Retorna los datos recibidos
-    } catch (error) {
-      console.error('Error:', error.response.status, error.response.data);
-      if (error.response.status === 400) {
-        throw new Error('El ID del proyecto es requerido.');
-      } else if (error.response.status === 401) {
-        throw new Error('No autenticado.');
-      } else {
-        throw error; // Lanza el error para que sea manejado por el llamador de la función
-      }
-    }
+async function createWatcher(req, res, next) {
+  const url = '/v3/watcher'
+
+  // Datos del nuevo watcher en formato JSON
+  const payload = {
+    name: 'Nombre del watcher',
+    description: 'Descripción del watcher',
+    address: 'Dirección del watcher',
+    network: 'Red del watcher',
+    type: 'MINED_TRANSACTION',
+    webhookUrl: 'URL del webhook',
+    confirmationsBlocks: 0
   }
 
+  try {
+    const response = await startonApi.post(url, payload)
+    console.log('Respuesta:', response.status, response.data)
+    res.json(response.data)
+  } catch (error) {
+    console.error('Error:', error.response.status, error.response.data)
+    next(error)
+  }
+}
 
-  async function resendWebhook(id) {
-    const url = `/v3/webhook/${id}/resend`;
-  
-    try {
-      const response = await startonApi.post(url);
-      console.log('Respuesta:', response.status, response.data);
-      return response.data; // Retorna los datos recibidos
-    } catch (error) {
-      console.error('Error:', error.response.status, error.response.data);
-      if (error.response.status === 400) {
-        throw new Error('El ID del proyecto es requerido.');
-      } else if (error.response.status === 401) {
-        throw new Error('No autenticado.');
-      } else if (error.response.status === 404) {
-        throw new Error('Webhook no encontrado.');
-      } else {
-        throw error; // Lanza el error para que sea manejado por el llamador de la función
-      }
-    }
+async function createWatcherOnManyNetworks(req, res, next) {
+  const url = '/v3/watcher/'
+
+  // Datos del nuevo watcher en formato JSON
+  const payload = {
+    networks: ['network1', 'network2', 'network3'], // aca van  las redes deseadas
+    name: 'Nombre del watchers',
+    description: 'Descripción de los watchers',
+    address: 'Dirección de los watchers',
+    type: 'MINED_TRANSACTION',
+    webhookUrl: 'URL del webhook',
+    confirmationsBlocks: 0
   }
 
-  async function cancelWebhook(id) {
-    const url = `/v3/webhook/${id}/cancel`;
-  
-    try {
-      const response = await startonApi.post(url);
-      console.log('Respuesta:', response.status, response.data);
-      return response.data; // Retorna los datos recibidos
-    } catch (error) {
-      console.error('Error:', error.response.status, error.response.data);
-      if (error.response.status === 400) {
-        throw new Error('El ID del proyecto es requerido.');
-      } else if (error.response.status === 401) {
-        throw new Error('No autenticado.');
-      } else if (error.response.status === 404) {
-        throw new Error('Webhook no encontrado.');
-      } else {
-        throw error; // Lanza el error para que sea manejado por el llamador de la función
-      }
-    }
+  try {
+    const response = await startonApi.post(url, payload)
+    console.log('Respuesta:', response.status, response.data)
+    res.json(response.data)
+  } catch (error) {
+    console.error('Error:', error.response.status, error.response.data)
+    next(error)
   }
-  
-  
+}
 
-  module.exports ={regenerateWebhookSigningSecret, resendWebhook,cancelWebhook}
+async function updateWatcher(req, res, next) {
+  const { id } = req.params
+  const url = `/v3/watcher/`
+
+  // Datos actualizados del watcher en formato JSON
+  const payload = {
+    paused: true, // (true o false) para pausar o reanudar el watcher
+    webhookUrl: 'URL actualizada del webhook', // Actualiza la URL del webhook
+    confirmationsBlocks: 5, // Actualiza el número de bloques de confirmación
+    name: 'Nombre actualizado del watcher', // Actualiza el nombre del watcher
+    description: 'Descripción actualizada del watcher' // Actualiza la descripción del watcher
+  }
+
+  try {
+    const response = await startonApi.patch(`${url}${id}`, payload)
+    console.log('Respuesta:', response.status, response.data)
+    res.json(response.data)
+  } catch (error) {
+    console.error('Error:', error.response.status, error.response.data)
+    next(error)
+  }
+}
+
+async function deleteWatcherById(req, res, next) {
+  const { id } = req.params
+  const url = `/v3/watcher/`
+
+  try {
+    const response = await startonApi.delete(`${url}${id}`)
+    console.log('Respuesta:', response.status, response.data)
+    res.sendStatus(200) // Enviar un estado de éxito
+  } catch (error) {
+    console.error('Error:', error.response.status, error.response.data)
+    next(error)
+  }
+}
+
+module.exports = {
+  createWatcher,
+  createWatcherOnManyNetworks,
+  updateWatcher,
+  deleteWatcherById
+}
