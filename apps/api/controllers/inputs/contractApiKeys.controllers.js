e28675f79bc26e80e536abfecbf6ff5dee3e66f4
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:41:05 -0300

    contract api keys- refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/inputs/contractApiKeys.controllers.js
+++ apps/api/controllers/inputs/contractApiKeys.controllers.js
@@ -1,36 +1,36 @@
 const startonApi = require('../../services/smartContractAxiosInstance')
 
 // Función para crear una nueva API key
-async function createApiKey(apiKeyData) {
-  const url = '/v3'
-
+async function createApiKey(req, res, next) {
   try {
+    const apiKeyData = req.body
+    const url = '/v3' // Reemplaza la URL adecuada para la creación de la API key
+
     const response = await startonApi.post(url, apiKeyData)
     console.log('Respuesta:', response.status, response.data)
     // Aquí puedes manejar la respuesta si es necesario
+    res.status(201).json(response.data) // Enviar un estado de éxito y la respuesta
   } catch (error) {
     console.error('Error:', error.response.status, error.response.data)
     // Aquí puedes manejar el error si ocurre
+    next(error) // Pasar el error al siguiente middleware para su manejo
   }
-  // Datos de la nueva API key en formato JSON ejemplo de objeto para enviar en la request
-  // const newApiKeyData = {
-  //   name: "Nombre de la nueva API key",
-  //   description: "Descripción de la nueva API key",
-  //   scope: {} // Puedes proporcionar un objeto vacío o agregar propiedades específicas según los permisos necesarios
-  // };
 }
 
-// Función para eliminar una API key por su ID
-async function deleteApiKeyById(id) {
-  const url = `/v3`
-
+// Función para eliminar una API key por su nombre
+async function deleteApiKeyById(req, res, next) {
   try {
+    const name = req.params.name
+    const url = `/v3/${name}` // Reemplaza la URL adecuada para la eliminación de la API key
+
     const response = await startonApi.delete(url)
     console.log('Respuesta:', response.status, response.data)
     // Aquí puedes manejar la respuesta si es necesario
+    res.sendStatus(200) // Enviar un estado de éxito
   } catch (error) {
     console.error('Error:', error.response.status, error.response.data)
     // Aquí puedes manejar el error si ocurre
+    next(error) // Pasar el error al siguiente middleware para su manejo
   }
 }
 
