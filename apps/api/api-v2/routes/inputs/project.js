93a1ea0d0cf9983f6248992fb7435a621497cb2d
FedericoA15 <federicoalmeida15@gmail.com>
2023-07-21 16:10:24 -0300

    add routas de validaciones project

--- apps/api/api-v2/routes/inputs/project.js
+++ apps/api/api-v2/routes/inputs/project.js
@@ -5,9 +5,10 @@ const { addProject } = require('../../controllers/project/add-project')
 const { updateProject } = require('../../controllers/project/update-project')
 const { patchProject } = require('../../controllers/project/patch-project')
 const { deleteProject } = require('../../controllers/project/delete-project')
+const validatePageMiddleware = require('../../middlewares/validation/page/pageValidation')
 
 routerProject
-  .post('/', addProject)
+  .post('/', validatePageMiddleware, addProject)
   .put('/:id', updateProject)
   .patch('/:id', patchProject)
   .delete('/:id', deleteProject)
