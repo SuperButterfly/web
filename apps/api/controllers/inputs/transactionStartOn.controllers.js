f5bce042540af73686bc16d02e7b256908c3ae8a
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:42:00 -0300

    transactions - refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/inputs/transactionStartOn.controllers.js
+++ apps/api/controllers/inputs/transactionStartOn.controllers.js
@@ -1,45 +1,29 @@
-const startonApi = require('../../services/smartContractAxiosInstance')
+const startonApi = require('../../services/smartContractAxiosInstance');
 
-// Datos ejemplo de la nueva transacción en formato JSON para crear la transacción
-// const newTransactionData = {
-//     data: "string",
-//     gasLimit: "string",
-//     speed: "low",
-//     customGas: {
-//       gasPrice: "string",
-//       maxFeePerGas: "string",
-//       maxPriorityFeePerGas: "string",
-//     },
-//     metadata: {},
-//     network: "string",
-//     nonce: 0,
-//     signerWallet: "string",
-//     to: "string",
-//     value: "string",
-//   };
-async function createTransaction(transactionData) {
-  const url = '/v3/transaction'
+async function createTransaction(req, res, next) {
+  const transactionData = req.body;
 
   try {
-    const response = await startonApi.post(url, transactionData)
-    console.log('Respuesta:', response.status, response.data)
-    return response.data // Retorna los datos recibidos de la nueva transacción
+    const url = '/v3/transaction';
+    const response = await startonApi.post(url, transactionData);
+    console.log('Respuesta:', response.status, response.data);
+    res.json(response.data);
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
+    console.error('Error:', error.response.status, error.response.data);
     if (error.response.status === 400) {
-      throw new Error('Solicitud incorrecta.')
+      throw new Error('Solicitud incorrecta.');
     } else if (error.response.status === 401) {
-      throw new Error('No autenticado.')
+      throw new Error('No autenticado.');
     } else if (error.response.status === 404) {
-      throw new Error('No encontrado.')
+      throw new Error('No encontrado.');
     } else if (error.response.status === 422) {
-      throw new Error('Entidad no procesable.')
+      throw new Error('Entidad no procesable.');
     } else if (error.response.status === 500) {
-      throw new Error('Error interno del servidor.')
+      throw new Error('Error interno del servidor.');
     } else {
-      throw error // Lanza el error para que sea manejado por el llamador de la función
+      next(error);
     }
   }
 }
 
-module.exports = { createTransaction }
+module.exports = { createTransaction };
