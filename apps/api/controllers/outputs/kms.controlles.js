d220b6fcc40bf6eae3ed76a1aebd7c59e83cbeef
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:42:32 -0300

    kms - refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/outputs/kms.controlles.js
+++ apps/api/controllers/outputs/kms.controlles.js
@@ -1,82 +1,84 @@
 const startonApi = require('../../services/smartContractAxiosInstance')
 
-async function getAllKMS() {
+async function getAllKMS(req, res, next) {
   const url = '/v3/kms'
 
   try {
     const response = await startonApi.get(url)
     console.log('Respuesta:', response.status, response.data)
-    return response.data
+    res.json(response.data)
   } catch (error) {
     console.error('Error:', error.response.status, error.response.data)
     if (error.response.status === 401) {
-      throw new Error('No autenticado.')
+      next(new Error('No autenticado.'))
     } else {
-      throw error
+      next(error)
     }
   }
 }
 
-async function getKMSById(id) {
+async function getKMSById(req, res, next) {
+  const { id } = req.params
   const url = `/v3/kms/${id}`
 
   try {
     const response = await startonApi.get(url)
     console.log('Respuesta:', response.status, response.data)
-    return response.data
+    res.json(response.data)
   } catch (error) {
     console.error('Error:', error.response.status, error.response.data)
     if (error.response.status === 401) {
-      throw new Error('No autenticado.')
+      next(new Error('No autenticado.'))
     } else if (error.response.status === 404) {
-      throw new Error('KMS no encontrado.')
+      next(new Error('KMS no encontrado.'))
     } else {
-      throw error
+      next(error)
     }
   }
 }
 
-async function getWallets(name, isExternal) {
+async function getWallets(req, res, next) {
   const url = '/v3/kms/wallet'
-
-  const params = {
-    name,
-    isExternal
-  }
+  const { name, isExternal } = req.query
+  const params = { name, isExternal }
 
   try {
     const response = await startonApi.get(url, { params })
     console.log('Respuesta:', response.status, response.data)
-    return response.data
+    res.json(response.data)
   } catch (error) {
     console.error('Error:', error.response.status, error.response.data)
     if (error.response.status === 401) {
-      throw new Error('No autenticado.')
+      next(new Error('No autenticado.'))
     } else {
-      throw error
+      next(error)
     }
   }
 }
 
+async function getWalletByAddress(req, res, next) {
+  const { address } = req.params
+  const url = `/v3/kms/wallet/${address}`
 
-async function getWalletByAddress(address) {
-    const url = `/v3/kms/wallet/${address}`;
-  
-    try {
-      const response = await startonApi.get(url);
-      console.log('Respuesta:', response.status, response.data);
-      return response.data;
-    } catch (error) {
-      console.error('Error:', error.response.status, error.response.data);
-      if (error.response.status === 401) {
-        throw new Error('No autenticado.');
-      } else if (error.response.status === 404) {
-        throw new Error('Wallet no encontrada.');
-      } else {
-        throw error;
-      }
+  try {
+    const response = await startonApi.get(url)
+    console.log('Respuesta:', response.status, response.data)
+    res.json(response.data)
+  } catch (error) {
+    console.error('Error:', error.response.status, error.response.data)
+    if (error.response.status === 401) {
+      next(new Error('No autenticado.'))
+    } else if (error.response.status === 404) {
+      next(new Error('Wallet no encontrada.'))
+    } else {
+      next(error)
     }
   }
-  
+}
 
-module.exports = { getAllKMS, getKMSById, getWallets, getWalletByAddress }
+module.exports = {
+  getAllKMS,
+  getKMSById,
+  getWallets,
+  getWalletByAddress
+}
