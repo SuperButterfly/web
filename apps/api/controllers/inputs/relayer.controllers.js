38cf66e344f4cf8f27755e87b0ba07e40feffa06
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:41:36 -0300

    relayer - refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/inputs/relayer.controllers.js
+++ apps/api/controllers/inputs/relayer.controllers.js
@@ -1,20 +1,22 @@
-const startonApi = require('../../services/smartContractAxiosInstance')
+const startonApi = require('../../services/smartContractAxiosInstance');
 
-async function updateRelayerSettingsForProject(network, settings) {
-  const url = `/v3`
+async function updateRelayerSettingsForProject(req, res, next) {
+  const { network } = req.params;
+  const settings = req.body;
+  const url = `/v3`;
 
   try {
-    const response = await startonApi.patch(url, settings)
-    console.log('Respuesta:', response.status, response.data)
-    return response.data // Retorna las configuraciones actualizadas del Relayer para el proyecto y red específicos
+    const response = await startonApi.patch(url, settings);
+    console.log('Respuesta:', response.status, response.data);
+    res.json(response.data); // Retorna las configuraciones actualizadas del Relayer para el proyecto y red específicos
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
+    console.error('Error:', error.response.status, error.response.data);
     if (error.response.status === 401) {
-      throw new Error('No autenticado.')
+      next(new Error('No autenticado.'));
     } else {
-      throw error // Lanza el error para que sea manejado por el llamador de la función
+      next(error); // Lanza el error para que sea manejado por el middleware de error
     }
   }
 }
 
-module.exports = { updateRelayerSettingsForProject }
+module.exports = { updateRelayerSettingsForProject };
