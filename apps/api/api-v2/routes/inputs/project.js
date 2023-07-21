f78fb23f5ee8927a93d3738e0fb3a460619b698e
AgustinChavero <agustindanielchavero@gmail.com>
2023-07-21 12:18:46 -0300

    Validacion para creacion de proyecto, cambio en rutas, y fix en controller

--- apps/api/api-v2/routes/inputs/project.js
+++ apps/api/api-v2/routes/inputs/project.js
@@ -6,8 +6,10 @@ const { updateProject } = require('../../controllers/project/update-project')
 const { patchProject } = require('../../controllers/project/patch-project')
 const { deleteProject } = require('../../controllers/project/delete-project')
 
+const projectValidationMiddleware = require('../../middlewares/validation/project/projectValidation')
+
 routerProject
-  .post('/', addProject)
+  .post('/', projectValidationMiddleware, addProject)
   .put('/:id', updateProject)
   .patch('/:id', patchProject)
   .delete('/:id', deleteProject)
