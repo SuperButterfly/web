4f1a2519071542ede9eb0a3e0c8f35e9051ddc78
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:44:15 -0300

    data - refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/routes/outputs/data.routes.js
+++ apps/api/routes/outputs/data.routes.js
@@ -1,14 +1,13 @@
-const { Router } = require('express')
-const dataRouter = Router()
-
+const { Router } = require('express');
+const dataRouter = Router();
 const {
   getGasPrice,
   getAddressBalance,
   getContractBalance
-} = require('../../controllers/outputs/ipfs.controllers.js')
+} = require('../../controllers/outputs/ipfs.controllers.js');
 
-dataRouter.get('/:network/address/:address/balance/native', getGasPrice)
-dataRouter.get('/:network/erc20/contractAddress/balance', getAddressBalance)
-dataRouter.get('/:network/address/:address/balance/native', getContractBalance)
+dataRouter.get('/:network/gas-price', getGasPrice);
+dataRouter.get('/:network/address/:address/balance/native', getAddressBalance);
+dataRouter.get('/:network/erc20/:contractAddress/balance', getContractBalance);
 
-module.exports = dataRouter
+module.exports = dataRouter;
