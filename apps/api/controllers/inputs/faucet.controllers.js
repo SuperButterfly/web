e8d1ff1f62a1a024fb94f7c116e183b0ed6d2e1c
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:41:17 -0300

    faucet- refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/inputs/faucet.controllers.js
+++ apps/api/controllers/inputs/faucet.controllers.js
@@ -1,22 +1,24 @@
-const startonApi = require('../../services/smartContractAxiosInstance')
+const startonApi = require('../../services/smartContractAxiosInstance');
 
-async function requestFaucet(network, wallet, captchaToken) {
-  const url = `/v3`
+async function requestFaucet(req, res, next) {
+  const { network } = req.params;
+  const { wallet, captchaToken } = req.body;
+
+  const url = `/v3/faucet/${network}`;
 
   const payload = {
     wallet,
     captchaToken,
-    network
-  }
+  };
 
   try {
-    const response = await startonApi.post(url, payload)
-    console.log('Respuesta:', response.status, response.data)
-    return response.data
+    const response = await startonApi.post(url, payload);
+    console.log('Respuesta:', response.status, response.data);
+    res.status(201).json(response.data);
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-    throw error
+    console.error('Error:', error.response.status, error.response.data);
+    next(error);
   }
 }
 
-module.exports = { requestFaucet }
+module.exports = { requestFaucet };
