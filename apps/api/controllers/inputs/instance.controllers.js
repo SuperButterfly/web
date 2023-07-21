0e4fd88376139d022e7266f7a7374b512a3ee229
Yixsi <yixsi3103@gmail.com>
2023-07-21 11:28:12 -0500

    test: send files or directory through ssh successful

--- apps/api/controllers/inputs/instance.controllers.js
+++ apps/api/controllers/inputs/instance.controllers.js
@@ -11,86 +11,87 @@ const postInstance = async (req, res) => {
   try {
     const { instanceInfo, sendFiles } = req.body
 
-    const instanceData = {
-      name: `/var/www/${instanceInfo?.name ?? 'new-instance'}`,
-      project: SCW_PROJECT_ID,
-      commercial_type: instanceInfo.type,
-      image: '544f0add-626b-4e4f-8a96-79fa4414d99a',
-      enable_ipv6: true,
-      volumes: {
-        0: {
-          name: 'my-volume',
-          size: instanceInfo.volumeSize,
-          volume_type: 'l_ssd'
-        }
-      }
+    // const instanceData = {
+    //   name: `/var/www/${instanceInfo?.name ?? 'new-instance'}`,
+    //   project: SCW_PROJECT_ID,
+    //   commercial_type: instanceInfo.type,
+    //   image: '544f0add-626b-4e4f-8a96-79fa4414d99a',
+    //   enable_ipv6: true,
+    //   volumes: {
+    //     0: {
+    //       name: 'my-volume',
+    //       size: instanceInfo.volumeSize,
+    //       volume_type: 'l_ssd'
+    //     }
+    //   }
+    // }
+
+    // // Create the instance in the SCW cloud
+    // const instanceResponse = await sendRequest('POST', '/servers', instanceData)
+    // const { id, name, volumes } = instanceResponse.server
+    // const volumeId = volumes['0'].id
+
+    // // Create an IP flexible and attach it to the instance
+
+    // const ipResponse = await sendRequest('POST', '/ips', {
+    //   project: SCW_PROJECT_ID,
+    //   server: id
+    // })
+
+    // const { ip } = ipResponse
+
+    // // Power on the instance
+    // await sendRequest('POST', `/servers/${id}/action`, { action: 'poweron' })
+
+    // // Wait for the instance to be fully powered on
+    // let instanceStatus = ''
+    // while (instanceStatus !== 'running') {
+    //   const instanceDetails = await sendRequest('GET', `/servers/${id}`)
+    //   instanceStatus = instanceDetails.server.state
+    //   console.log(instanceStatus)
+
+    //   if (instanceStatus !== 'running') {
+    //     await wait(5000) // Wait for 5 seconds before checking again
+    //   }
+    // }
+
+    // // Create the instance in the DB
+    // const newInstance = await Instance.create(
+    //   { id, name, volumeId, ipID: ip.id, ipAddress: ip.address },
+    //   { id, name, volumeId, ipID: ip.id, ipAddress: ip.address }
+    // )
+
+    // // Set the relation with the project
+    // if (instanceInfo.id) {
+    //   const template = await Template.findByPk(instanceInfo.projectId)
+    //   if (!template) throw new Error('Template not found')
+    //   await newInstance.setTemplate(template.id)
+    // }
+
+    // newInstance.save()
+
+    // if (sendFiles && instanceStatus === 'running') {
+
+    const config = {
+      host: '51.15.212.56',
+      username: 'root',
+      privateKeyPath: PRIVATE_KEY,
+      passphrase: PASS_PHRASE
     }
 
-    // Create the instance in the SCW cloud
-    const instanceResponse = await sendRequest('POST', '/servers', instanceData)
-    const { id, name, volumes } = instanceResponse.server
-    const volumeId = volumes['0'].id
+    const ssh = new SSHUtility()
 
-    // Create an IP flexible and attach it to the instance
+    await ssh.connect(config)
+    const localFilePath = path.resolve(__dirname, '../../services')
+    const remoteDirectory = '/root/test'
+    await ssh.transferFiles(localFilePath, remoteDirectory)
 
-    const ipResponse = await sendRequest('POST', '/ips', {
-      project: SCW_PROJECT_ID,
-      server: id
-    })
+    const remoteDirectoryToList = '/root/test'
+    await ssh.listRemoteMachineFiles(remoteDirectoryToList)
+    // }
 
-    const { ip } = ipResponse
-
-    // Power on the instance
-    await sendRequest('POST', `/servers/${id}/action`, { action: 'poweron' })
-
-    // Wait for the instance to be fully powered on
-    let instanceStatus = ''
-    while (instanceStatus !== 'running') {
-      const instanceDetails = await sendRequest('GET', `/servers/${id}`)
-      instanceStatus = instanceDetails.server.state
-      console.log(instanceStatus)
-
-      if (instanceStatus !== 'running') {
-        await wait(5000) // Wait for 5 seconds before checking again
-      } 
-    }
-
-    // Create the instance in the DB
-    const newInstance = await Instance.create(
-      { id, name, volumeId, ipID: ip.id, ipAddress: ip.address },
-      { id, name, volumeId, ipID: ip.id, ipAddress: ip.address }
-    )
-
-    // Set the relation with the project
-    if (instanceInfo.id) {
-      const template = await Template.findByPk(instanceInfo.projectId)
-      if (!template) throw new Error('Template not found')
-      await newInstance.setTemplate(template.id)
-    }
-
-    newInstance.save()
-
-    if (sendFiles && instanceStatus === 'running') {
-      const sshUtility = new SSHUtility()
-
-      const config = {
-        host: ip.address,
-        username: 'root',
-        port: 22,
-        privateKeyPath: PRIVATE_KEY,
-        passphrase: PASS_PHRASE
-      }
-      const localFilePath = path.resolve(__dirname, './class.controllers.js')
-      const remoteDirectory = '/root/test'
-      sshUtility.connectAndTransferFiles(config, localFilePath, remoteDirectory)
-
-      const remoteDirectoryToList = '/root/test'
-      sshUtility.listRemoteMachineFiles(remoteDirectoryToList)
-    }
-
-    res
-      .status(201)
-      .json({ message: 'Instance created successfully', instance: newInstance }) 
+    await ssh.disconnect()
+    res.status(201).json({ message: 'Instance created successfully' }) // , instance: newInstance
   } catch (error) {
     console.log(error)
     res
@@ -125,103 +126,3 @@ const updateInstance = async (req, res) => {
 }
 
 module.exports = { postInstance, updateInstance }
-/** const { readFileSync } = require('fs')
-const { Client } = require('ssh2')
-
-const postInstance = async (req, res) => {
-  try {
-    // ... (your other code)
-
-    const conn = new Client()
-
-    conn.on('ready', () => {
-      conn.sftp((err, sftp) => {
-        if (err) throw err
-
-        const localPath = path.resolve(__dirname, '../../componentsJson/Banner1.json')
-        const remotePath = '/root/Banner1.json'
-
-        const readStream = readFileSync(localPath)
-        const writeStream = sftp.createWriteStream(remotePath)
-
-        // Initiate the file transfer
-        readStream.pipe(writeStream)
-
-        // Close the connection when the transfer is complete
-        writeStream.on('close', () => {
-          console.log('File transferred successfully')
-          conn.end()
-          res.status(201).json({ message: 'successfully' })
-        })
-      })
-    })
-
-    conn.connect({
-      host: '51.15.218.113',
-      username: 'root',
-      privateKey: readFileSync('C:\\Users\\YISNEY SOTO\\keys-ssh'),
-      passphrase: 'test-code'
-    })
-  } catch (error) {
-    console.log(error)
-    res
-      .status(500)
-      .json({ error: 'Error establishing connection', message: error.message })
-  }
-} */
-
-// conn.exec('cat ~/.ssh/authorized_keys', (err, stream) => {
-//   if (err) throw err
-//   stream
-//     .on('close', (code, signal) => {
-//       conn.end()
-//     })
-//     .on('data', (data) => {
-//       console.log('STDOUT: ' + data)
-//     })
-//     .stderr.on('data', (data) => {
-//       console.log('STDERR: ' + data)
-//     })
-// })
-
-// conn.exec('ls -l -1 -a', (err, stream) => {
-//   if (err) throw err
-//   stream
-//     .on('close', (code, signal) => {
-//       conn.end()
-//     })
-//     .on('data', (data) => {
-//       console.log('STDOUT: ' + data)
-//     })
-//     .stderr.on('data', (data) => {
-//       console.log('STDERR: ' + data)
-//     })
-// })
-
-// NODE - SSH
-
-/**
- await ssh.connect({
-      host: '51.15.212.56',
-      username: 'root',
-      privateKeyPath: 'C:\\Users\\YISNEY SOTO\\keys-ssh',
-      passphrase: 'test-code'
-    })
-    await ssh.mkdir('/root/test')
-
-    await ssh.putFile(
-      path.resolve(__dirname, './instance.controllers.js'),
-      '/root/test/instance.controllers.js'
-    )
-
-  const result = await ssh.execCommand('cat /root/test/instance.controllers.js')
-
-    console.log({ result });
-
-    const remoteDirectory = '/root/test';
-    const resultDirectory = await ssh.execCommand(`ls -l ${remoteDirectory}`);
-
-    console.log('Files in remote directory:', resultDirectory.stdout);
-
-    ssh.dispose()
- */
--- apps/api/utils/ssh.js
+++ apps/api/utils/ssh.js
@@ -9,10 +9,23 @@ class SSHUtility {
     this.ssh = new NodeSSH()
   }
 
-  async connectAndTransferFiles(config, localPath, remoteDirectory) {
+  async connect(config) {
     try {
-      console.log(config)
       await this.ssh.connect(config)
+      console.log('Connected to the SSH server:', config.host)
+    } catch (error) {
+      console.error('Error connecting to the SSH server:', error.message)
+      throw new Error('SSH connection failed')
+    }
+  }
+
+  async disconnect() {
+    this.ssh.dispose()
+    console.log('SSH connection closed')
+  }
+
+  async transferFiles(localPath, remoteDirectory) {
+    try {
 
       await this.ssh.mkdir(remoteDirectory)
 
@@ -28,7 +41,7 @@ class SSHUtility {
           const files = await fs.readdir(localPath)
           for (const file of files) {
             const filePath = path.join(localPath, file)
-            await transferFileRecursive(filePath, remoteDirPath) // Using the arrow function directly
+            await transferFileRecursive(filePath, remoteDirPath)
           }
         } else {
           const localFileName = path.basename(localPath)
@@ -38,23 +51,33 @@ class SSHUtility {
       }
 
       await transferFileRecursive(localPath, remoteDirectory)
-
-      this.ssh.dispose()
     } catch (error) {
       throw new Error(error)
     }
   }
 
-  async listRemoteMachineFiles() {
+  async listRemoteMachineFiles(remoteDirectory) {
     try {
-      const remoteDirectory = '/root/test'
       const resultDirectory = await this.ssh.execCommand(
         `ls -l ${remoteDirectory}`
       )
 
       console.log('Files in remote directory:', resultDirectory.stdout)
+
+      await this.disconnect()
     } catch (error) {
       console.error('Error:', error.message)
+    }
+  }
+
+  async addSSHKeyToAuthorizedKeys(sshPublicKey, remoteUsername) {
+    try {
+      const command = `echo '${sshPublicKey}' >> ~/.ssh/authorized_keys`;
+      await this.ssh.execCommand(command, { cwd: `/home/${remoteUsername}` });
+      console.log('SSH public key added to authorized_keys');
+    } catch (error) {
+      console.error('Error adding SSH public key to authorized_keys:', error.message);
+      throw new Error('Failed to add SSH public key to authorized_keys');
     }
   }
 }
