65a7911f49557f576728af72e8b7068901a652df
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:43:46 -0300

    kms - refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/routes/inputs/kms.routes.js
+++ apps/api/routes/inputs/kms.routes.js
@@ -1,5 +1,5 @@
-const { Router } = require('express')
-const faucetRouter = Router()
+const { Router } = require('express');
+const faucetRouter = Router();
 
 const {
   createKMS,
@@ -8,13 +8,13 @@ const {
   deleteKMSById,
   createWallet,
   updateWalletByAddress
-} = require('../../controllers/inputs/faucet.controllers.js')
+} = require('../../controllers/inputs/faucet.controllers.js');
 
-faucetRouter.post('/', createKMS)
-faucetRouter.post('/', createWallet)
-faucetRouter.patch('/', updateKMSById)
-faucetRouter.patch('/', updateWalletByAddress)
-faucetRouter.delete('/', deleteKMSById)
-faucetRouter.delete('/', deleteWalletByAddress)
+faucetRouter.post('/kms', createKMS);
+faucetRouter.post('/wallet', createWallet);
+faucetRouter.patch('/kms/:id', updateKMSById);
+faucetRouter.patch('/wallet/:address', updateWalletByAddress);
+faucetRouter.delete('/kms/:id', deleteKMSById);
+faucetRouter.delete('/wallet/:address', deleteWalletByAddress);
 
-module.exports = faucetRouter
+module.exports = faucetRouter;
