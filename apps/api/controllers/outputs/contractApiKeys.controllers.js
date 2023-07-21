9e8572425d0f5041914f5fd6f62026de3b7fc03b
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-21 14:42:21 -0300

    contract api key- refactorizado de controller y router en formato middleware de express para manejar req/res y normalizar el formato de codigo con el resto del backend

--- apps/api/controllers/outputs/contractApiKeys.controllers.js
+++ apps/api/controllers/outputs/contractApiKeys.controllers.js
@@ -1,20 +1,21 @@
-const startonApi = require('../../services/smartContractAxiosInstance')
+const startonApi = require('../../services/smartContractAxiosInstance');
 
 // Función para obtener la lista de todas las API Keys existentes
-async function getAllApiKeys(name) {
-  const url = '/v3'
+async function getAllApiKeys(req, res, next) {
+  const url = '/v3';
 
   // Parámetros de consulta (query parameters) para filtrar por nombre si se proporciona
-  const params = name ? { name } : {}
+  const { name } = req.query;
+  const params = name ? { name } : {};
 
   try {
-    const response = await startonApi.get(url, { params })
-    console.log('Respuesta:', response.status, response.data)
-    // Aquí puedes manejar la respuesta si es necesario
+    const response = await startonApi.get(url, { params });
+    console.log('Respuesta:', response.status, response.data);
+    res.json(response.data);
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-    // Aquí puedes manejar el error si ocurre
+    console.error('Error:', error.response.status, error.response.data);
+    next(error);
   }
 }
 
-module.exports = { getAllApiKeys }
+module.exports = { getAllApiKeys };
