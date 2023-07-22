630c497d743073106593f7c98a7472bb9cc0f2f3
Pablo Amico <pablosecuen@gmail.com>
2023-07-22 10:30:36 -0300

    Update contractApiKeys.controllers.js
    manejo de erores optimizado y agregado destructuring

--- apps/api/controllers/inputs/contractApiKeys.controllers.js
+++ apps/api/controllers/inputs/contractApiKeys.controllers.js
@@ -1,38 +1,48 @@
 const startonApi = require('../../services/smartContractAxiosInstance')
 
-// Función para crear una nueva API key
 async function createApiKey(req, res, next) {
   try {
-    const apiKeyData = req.body
-    const url = '/v3' // Reemplaza la URL adecuada para la creación de la API key
+    const { apiKeyData } = req.body;
+    const url = '/v3'; // Reemplaza la URL adecuada para la creación de la API key
 
-    const response = await startonApi.post(url, apiKeyData)
-    console.log('Respuesta:', response.status, response.data)
+    const response = await startonApi.post(url, apiKeyData);
+    console.log('Respuesta:', response.status, response.data);
     // Aquí puedes manejar la respuesta si es necesario
-    res.status(201).json(response.data) // Enviar un estado de éxito y la respuesta
+    res.status(201).json(response.data); // Enviar un estado de éxito y la respuesta
   } catch (error) {
-     console.error('Error:', error.response.status, error.response.data)
-      res.status(500).json({ error: 'Error interno del servidor' });
-    // Aca vamos a manejar el error cuando ocurra
-    next(error) // Pasar el error al siguiente middleware para su manejo
+    console.error('Error:', error.response.status, error.response.data);
+    // Aquí puedes manejar el error si ocurre
+    if (error.response.status === 401) {
+      res.status(401).json('No autenticado.');
+    } else if (error.response.status === 403) {
+      res.status(403).json('Acceso prohibido.');
+    } else if (error.response.status === 409) {
+      res.status(409).json('Conflicto al procesar la solicitud.');
+    } else {
+      res.status(500).json('Error interno del servidor.');
+    }
   }
 }
 
-// Función para eliminar una API key por su nombre
 async function deleteApiKeyById(req, res, next) {
   try {
-    const name = req.params.name
-    const url = `/v3/${name}` // Reemplaza la URL adecuada para la eliminación de la API key
+    const { name } = req.params;
+    const url = `/v3/${name}`; // Reemplaza la URL adecuada para la eliminación de la API key
 
-    const response = await startonApi.delete(url)
-    console.log('Respuesta:', response.status, response.data)
+    const response = await startonApi.delete(url);
+    console.log('Respuesta:', response.status, response.data);
     // Aquí puedes manejar la respuesta si es necesario
-    res.sendStatus(200) // Enviar un estado de éxito
+    res.sendStatus(200); // Enviar un estado de éxito
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-      res.status(500).json({ error: 'Error interno del servidor' });
-     // Aca vamos a manejar el error cuando ocurra
-    next(error) // Pasar el error al siguiente middleware para su manejo
+    console.error('Error:', error.response.status, error.response.data);
+    // Aquí puedes manejar el error si ocurre
+    if (error.response.status === 401) {
+      res.status(401).json('No autenticado.');
+    } else if (error.response.status === 404) {
+      res.status(404).json('No encontrado.');
+    } else {
+      res.status(500).json('Error interno del servidor.');
+    }
   }
 }
 
