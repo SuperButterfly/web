2ca51b2cb22cf8371bc44dcfd168d8d0bf7ce983
Pablo Amico <pablosecuen@gmail.com>
2023-07-22 10:43:48 -0300

    Update kms.routes.js
    correccion de las rutas para ordenar las :id a lo ultimo

--- apps/api/routes/inputs/kms.routes.js
+++ apps/api/routes/inputs/kms.routes.js
@@ -12,9 +12,9 @@ const {
 
 faucetRouter.post('/kms', createKMS);
 faucetRouter.post('/wallet', createWallet);
-faucetRouter.patch('/kms/:id', updateKMSById);
 faucetRouter.patch('/wallet/:address', updateWalletByAddress);
-faucetRouter.delete('/kms/:id', deleteKMSById);
 faucetRouter.delete('/wallet/:address', deleteWalletByAddress);
+faucetRouter.delete('/kms/:id', deleteKMSById);
+faucetRouter.patch('/kms/:id', updateKMSById);
 
 module.exports = faucetRouter;
