e433c640a0a307da713b0ad5256039ffe163aab9
Pablo Amico <pablosecuen@gmail.com>
2023-07-22 10:39:49 -0300

    Update webHookStartOn.controllers.js
    segun lo mencionado por federico el payload viene con destructuring por body y luego asiganmos ese objeto completo, si bien podriamos guardar todo en una variable payload de esta manera tenemos mas controls sobre los datos que se manejan dentro, son solo 5 lineas de codigo con muy pocos caracteres, y cualquier otro developer que venga a ver el codigo va a entender mejor como esta constituido este payload que puede ser algo complejo

--- apps/api/controllers/inputs/webHookStartOn.controllers.js
+++ apps/api/controllers/inputs/webHookStartOn.controllers.js
@@ -1,87 +1,103 @@
 const startonApi = require('../../services/smartContractAxiosInstance')
 
 async function createWatcher(req, res, next) {
-  const url = '/v3/watcher'
+  const url = '/v3/watcher';
+  const { name, description, address, network, type, webhookUrl, confirmationsBlocks } = req.body;
 
-  // Datos del nuevo watcher en formato JSON
   const payload = {
-    name: 'Nombre del watcher',
-    description: 'Descripción del watcher',
-    address: 'Dirección del watcher',
-    network: 'Red del watcher',
-    type: 'MINED_TRANSACTION',
-    webhookUrl: 'URL del webhook',
-    confirmationsBlocks: 0
-  }
+    name,
+    description,
+    address,
+    network,
+    type,
+    webhookUrl,
+    confirmationsBlocks
+  };
 
   try {
-    const response = await startonApi.post(url, payload)
-    console.log('Respuesta:', response.status, response.data)
-    res.json(response.data)
+    const response = await startonApi.post(url, payload);
+    console.log('Respuesta:', response.status, response.data);
+    res.status(200).json(response.data);
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-    next(error)
+    console.error('Error:', error.response.status, error.response.data);
+    if (error.response.status === 401) {
+      res.status(401).json(error.message);
+    } else {
+      res.status(500).json(error.message);
+    }
   }
 }
 
 async function createWatcherOnManyNetworks(req, res, next) {
-  const url = '/v3/watcher/'
+  const url = '/v3/watcher/';
+  const { networks, name, description, address, type, webhookUrl, confirmationsBlocks } = req.body;
 
-  // Datos del nuevo watcher en formato JSON
   const payload = {
-    networks: ['network1', 'network2', 'network3'], // aca van  las redes deseadas
-    name: 'Nombre del watchers',
-    description: 'Descripción de los watchers',
-    address: 'Dirección de los watchers',
-    type: 'MINED_TRANSACTION',
-    webhookUrl: 'URL del webhook',
-    confirmationsBlocks: 0
-  }
+    networks,
+    name,
+    description,
+    address,
+    type,
+    webhookUrl,
+    confirmationsBlocks
+  };
 
   try {
-    const response = await startonApi.post(url, payload)
-    console.log('Respuesta:', response.status, response.data)
-    res.json(response.data)
+    const response = await startonApi.post(url, payload);
+    console.log('Respuesta:', response.status, response.data);
+    res.status(200).json(response.data);
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-    next(error)
+    console.error('Error:', error.response.status, error.response.data);
+    if (error.response.status === 401) {
+      res.status(401).json(error.message);
+    } else {
+      res.status(500).json(error.message);
+    }
   }
 }
 
 async function updateWatcher(req, res, next) {
-  const { id } = req.params
-  const url = `/v3/watcher/`
+  const { id } = req.params;
+  const url = `/v3/watcher/`;
+  const { paused, webhookUrl, confirmationsBlocks, name, description } = req.body;
 
-  // Datos actualizados del watcher en formato JSON
   const payload = {
-    paused: true, // (true o false) para pausar o reanudar el watcher
-    webhookUrl: 'URL actualizada del webhook', // Actualiza la URL del webhook
-    confirmationsBlocks: 5, // Actualiza el número de bloques de confirmación
-    name: 'Nombre actualizado del watcher', // Actualiza el nombre del watcher
-    description: 'Descripción actualizada del watcher' // Actualiza la descripción del watcher
-  }
+    paused,
+    webhookUrl,
+    confirmationsBlocks,
+    name,
+    description
+  };
 
   try {
-    const response = await startonApi.patch(`${url}${id}`, payload)
-    console.log('Respuesta:', response.status, response.data)
-    res.json(response.data)
+    const response = await startonApi.patch(`${url}${id}`, payload);
+    console.log('Respuesta:', response.status, response.data);
+    res.status(200).json(response.data);
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-    next(error)
+    console.error('Error:', error.response.status, error.response.data);
+    if (error.response.status === 401) {
+      res.status(401).json(error.message);
+    } else {
+      res.status(500).json(error.message);
+    }
   }
 }
 
 async function deleteWatcherById(req, res, next) {
-  const { id } = req.params
-  const url = `/v3/watcher/`
+  const { id } = req.params;
+  const url = `/v3/watcher/`;
 
   try {
-    const response = await startonApi.delete(`${url}${id}`)
-    console.log('Respuesta:', response.status, response.data)
-    res.sendStatus(200) // Enviar un estado de éxito
+    const response = await startonApi.delete(`${url}${id}`);
+    console.log('Respuesta:', response.status, response.data);
+    res.sendStatus(200); // Enviar un estado de éxito
   } catch (error) {
-    console.error('Error:', error.response.status, error.response.data)
-    next(error)
+    console.error('Error:', error.response.status, error.response.data);
+    if (error.response.status === 401) {
+      res.status(401).json(error.message);
+    } else {
+      res.status(500).json(error.message);
+    }
   }
 }
 
