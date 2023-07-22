d3e1ecbb154e0e43c7359161426a4ea7213aa02c
Pablo Amico <pablosecuen@gmail.com>
2023-07-22 10:41:13 -0300

    Update ipfs.routes.js
    orden de las rutas cambiados, lo que son /:id van al final de las rutas

--- apps/api/routes/inputs/ipfs.routes.js
+++ apps/api/routes/inputs/ipfs.routes.js
@@ -11,10 +11,10 @@ const {
 } = require('../../controllers/outputs/ipfs.controllers.js')
 
 ipfsRouter.post('/pin', pinFileToIPFS)
-ipfsRouter.patch('/pin/:id', updatePinnedFileById)
-ipfsRouter.delete('/pin/:id', deletePinnedFileById)
 ipfsRouter.post('/file', uploadFileOnIPFS)
 ipfsRouter.post('/folder', uploadFolderOnIPFS)
 ipfsRouter.post('/json', uploadJSONOnIPFS)
+ipfsRouter.patch('/pin/:id', updatePinnedFileById)
+ipfsRouter.delete('/pin/:id', deletePinnedFileById)
 
 module.exports = ipfsRouter
