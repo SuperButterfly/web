dd2e64a6a7d426f59b11b3cc9a260df5510d2082
Roger Miño <roger.rm03@gmail.com>
2023-07-24 19:31:08 -0300

    #325213312

---
+++ apps/client/src/Pages/dashboard/Editor/componentsCommunityScreen/categories/index.js
@@ -0,0 +1,17 @@
+import CRM from './Icons/CRM.png'
+import Editor from './Icons/Editor.png'
+import Office from './Icons/Office.png'
+import RedesSociales from './Icons/RedesSociales.png'
+import SEO from './Icons/SEO.png'
+import Shop from './Icons/Shop.png'
+//C:\Users\ROGER\Desktop\Aythen Web\web\apps\client\src\Pages\dashboard\Editor\componentsCommunityScreen\categories\Icons\Editor.png
+const icons = {
+    crm: CRM,
+    editor: Editor,
+    office: Office,
+    redesSociales:RedesSociales,
+    seo:SEO,
+    shop: Shop
+}
+
+export default icons
--- apps/client/src/Pages/dashboard/Editor/componentsCommunityScreen/navbar/Navbar.js
+++ apps/client/src/Pages/dashboard/Editor/componentsCommunityScreen/navbar/Navbar.js
@@ -1,10 +1,28 @@
 import './navbar.css'
 import SearchBar from '../../../../../Components/SearchBar/SearchBar'
-const Navbar = ()=>{
-
+import Icons from '../../componentsCommunityScreen/categories/index.js'
+const Navbar = ({setCategories})=>{
+    const hadleClick = ev =>{
+        setCategories(state=>{
+            return{
+                ...state,
+                [ev.target.name]:Icons[ev.target.name]
+            }
+        })
+    }
     return(
         <div className={"navbarContainer"}>
-            <SearchBar/>
+            <SearchBar bgColor="#FFF"/>
+            <div className='categoriesIconsContainer'>
+            {
+                Object.keys(Icons).map(icon=><img 
+                    onClick={hadleClick}
+                    name={icon}
+                    className='imgIcons' 
+                    src={Icons[icon]}
+                />)
+            }
+            </div>
         </div>
         
     )
