49bd836ce5dbf3433c29ab401a82725df8e175eb
AgustinChavero <agustindanielchavero@gmail.com>
2023-07-24 15:38:13 -0300

    #50421 modificacion de endpoint get-all-workspace

--- apps/api/api-v2/controllers/workspace/get-all-workspace.js
+++ apps/api/api-v2/controllers/workspace/get-all-workspace.js
@@ -3,9 +3,48 @@ const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
 const getAllWorkSpace = async (req, res, next) => {
-  const workspaces = await models.WorkSpaceModel.findAll()
-  if (!workspaces) throw new ClientError('Error al traer los workspaces', 400)
-  response(res, 200, workspaces)
+  const { userId } = req.body
+
+  const workspaces = await models.UserWorkSpaceModel.findAll({
+    where: { userId }
+  })
+
+  if (!workspaces) throw new ClientError('Error not found workspaces', 400)
+
+  const workspaceDetails = await Promise.all(
+    workspaces.map((workspace) =>
+      models.WorkSpaceModel.findByPk(workspace.workSpaceId)
+    )
+  )
+
+  if (!workspaceDetails)
+    throw new ClientError('Error not found workspaces', 400)
+
+  const usersDetails = []
+
+  await Promise.all(
+    workspaces.map(async (workspace) => {
+      const userFinded = await models.UserModel.findByPk(workspace.userId)
+      if (
+        userFinded &&
+        !usersDetails.some((user) => user.id === userFinded.id)
+      ) {
+        const userDefinite = {
+          id: userFinded.id,
+          userName: userFinded.userName,
+          email: userFinded.email,
+          plan: userFinded.plan,
+          resourcesList: userFinded.resourcesList,
+          theme: userFinded.theme,
+          billingDates: userFinded.billingDates,
+          isDeleted: userFinded.isDeleted
+        }
+        usersDetails.push(userDefinite)
+      }
+    })
+  )
+
+  response(res, 200, { workspaceDetails, usersDetails })
 }
 
 module.exports = { getAllWorkSpace: catchedAsync(getAllWorkSpace) }
