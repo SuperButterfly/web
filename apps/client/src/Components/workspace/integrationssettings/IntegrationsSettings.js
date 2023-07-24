24c2ee3ba8dc11d4079d07e9a58ebf23ffca870c
httpfido <fidel.romano.2016@hotmail.com>
2023-07-24 11:13:55 -0300

    #10 Mejora Css integrations: integrationssettings

--- apps/client/src/Components/workspace/integrationssettings/IntegrationsSettings.js
+++ apps/client/src/Components/workspace/integrationssettings/IntegrationsSettings.js
@@ -46,14 +46,37 @@ const IntegrationsSettings = () => {
           <span className="settings-general-heading-name">
             Other integrations
           </span>
-          <span className="settings-general-heading-name">GitHub</span>
+
+          <div className="integrations-github-container">
+            <div className="integrations-github-svg-container">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="github-svg"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="#2c3e50"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+              </svg>
+            </div>
+            <span className="settings-general-heading-name">GitHub</span>
+          </div>
           <div className="integrations-settings-text-container">
             <p className="settings-general-name-text">
               You can find your token in your
             </p>
+
             <a
               href="https://github.com/"
               className="settings-general-name-text"
+              rel='noreferrer'
+              target="_blank"
             >
               Github account
             </a>
