2c07449a647c1d988c00be601603df4dd42182ec
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-25 15:05:12 -0300

    #50426 set user to workspace
    crear un endpoint para que un usuario pueda asignar a otro usuario con distintos roles en un workspace

---
+++ apps/api/api-v2/controllers/workspace/set-user-to-workspace.js
@@ -0,0 +1,21 @@
+const { models } = require('../../database/connection/database')
+const { catchedAsync, response } = require('../../utils/err')
+const { ClientError } = require('../../utils/err/errors')
+
+const setUserToWorkSpace = async (req, res, next) => {
+    const { userToolId, workSpaceId, role } = req.body
+
+    const user = await models.UserModel.findByPk(userToolId)
+
+    if (!user) throw new ClientError('Error not found user', 400)
+    const workspace = await models.WorkSpaceModel.findByPk(workSpaceId)
+    if (!workspace) throw new ClientError('Error not found workspace', 400)   
+    await models.UserWorkSpaceModel.create({
+        userId: user.id,
+        workSpaceId: workspace.id,
+        role
+    })
+  response(res, 200, workspace)
+}
+
+module.exports = { setUserToWorkSpace: catchedAsync(setUserToWorkSpace) }
