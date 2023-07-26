a11909c23235cbe4a994a3f08e8db714c47d38ef
Pablo Salituri <pablo_salituri@live.com.ar>
2023-07-26 10:22:49 -0300

    Select cell y row resize fixed

--- apps/client/src/Pages/dashboard/Editor/dataTablesScreen/Table/ResizableRows/ResizableRows.jsx
+++ apps/client/src/Pages/dashboard/Editor/dataTablesScreen/Table/ResizableRows/ResizableRows.jsx
@@ -1,8 +1,11 @@
-import React, { memo, useEffect, useState } from 'react'
-import style from './resizableRows.module.css'
-import { handleRowSelect } from '../../../../../../redux/slices/datatableSlices'
-import Cell from '../Cell/Cell'
-import { useDispatch } from 'react-redux'
+/* //!BREAKPOINT */
+/* //!BREAKPOINT2 */
+
+import React, { memo, useEffect, useState } from 'react';
+import style from './resizableRows.module.css';
+import { handleRowSelect } from '../../../../../../redux/slices/datatableSlices';
+import Cell from '../Cell/Cell';
+import { useDispatch } from 'react-redux';
 
 const ResizableRow = memo(function ({
   row,
@@ -10,71 +13,64 @@ const ResizableRow = memo(function ({
   selected,
   sheet,
   handlers,
-  children
+  children,
 }) {
-  const [rowHeights, setRowHeights] = useState([])
-  // const [hoveredRowIndex, setHoveredRowIndex] = useState(-1)
-  const dispatch = useDispatch()
-  const onMouseDown = (e) => handleRowResize(e, rowIndex)
-  /* const handleMouseDown = (e) => {
-      e.stopPropagation();
-      onMouseDown(e);
-    }; */
+  
+  const [rowHeights, setRowHeights] = useState([]);
+  const dispatch = useDispatch();
 
   const handleSelect = (e) => {
-    const rowIndex = selected ? null : parseInt(e, 10)
-    dispatch(handleRowSelect(rowIndex))
-    // setHoveredRowIndex(rowIndex)
-  }
+    const rowIndex = selected ? null : parseInt(e, 10);
+    dispatch(handleRowSelect(rowIndex));
+  };
 
   const handleRowResize = (event, index) => {
-    event.preventDefault() // Prevent text selection
-    const startY = event.clientY
-    const startHeight = rowHeights[index]
+    event.preventDefault(); // Prevent text selection
+    const startY = event.clientY;
+    const startHeight = rowHeights[index];
 
     const handleMouseMove = (e) => {
-      const diff = e.clientY - startY
-      const newHeight = startHeight + diff
+      const diff = e.clientY - startY;
+      const newHeight = startHeight + diff;
       if (newHeight >= 30) {
-        const newHeights = [...rowHeights]
-        newHeights[index] = newHeight
-        setRowHeights(newHeights)
+        const newHeights = [...rowHeights];
+        newHeights[index] = newHeight;
+        setRowHeights(newHeights);
       }
-    }
+    };
 
     const handleMouseUp = () => {
-      document.removeEventListener('mousemove', handleMouseMove)
-      document.removeEventListener('mouseup', handleMouseUp)
-    }
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
 
-    document.addEventListener('mousemove', handleMouseMove)
-    document.addEventListener('mouseup', handleMouseUp)
-  }
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  };
 
   const handleCellMouseDown = (e) => {
-    const boundingRect = e.target.getBoundingClientRect()
-    const y = e.clientY - boundingRect.top
-    const bottomMargin = 12 // Adjust this value to set the margin from the bottom
+    const boundingRect = e.target.getBoundingClientRect();
+    const y = e.clientY - boundingRect.top;
+    const bottomMargin = 12; // Adjust this value to set the margin from the bottom
 
     if (y >= boundingRect.height - bottomMargin) {
       // Activate the row resize only if the cursor is close to the bottom edge
-      e.stopPropagation()
-      onMouseDown(e)
+      e.stopPropagation();
+      handleRowResize(e, rowIndex);
     }
-  }
+  };
 
   useEffect(() => {
-    console.log('TrackRender>ResizableRowsssssss')
-    setRowHeights(Array(sheet.getData().length).fill(30))
-  }, [sheet])
+    const initialHeights = Array(sheet.getData().length).fill(30);
+    setRowHeights(initialHeights);
+  }, [sheet]);
 
   return (
-    <tr
-      className={style.tr}
-      style={{ height: `${rowHeights[rowIndex]}px` }}
-      onMouseDown={(e) => handleRowResize(e, rowIndex)}
-    >
-      <td className={style.rowNumber}>
+    <tr className={style.tr} style={{ height: `${rowHeights[rowIndex]}px` }}>
+      <td
+        className={style.rowNumber}
+        onMouseDown={handleCellMouseDown}
+      >
         <input
           className={`${style.input} ${style.rowNumber} ${
             selected ? style.titleColumn : ''
@@ -82,7 +78,6 @@ const ResizableRow = memo(function ({
           type="text"
           value={rowIndex + 1}
           onClick={(e) => handleSelect(e.target.value)}
-          onMouseDown={handleCellMouseDown}
           readOnly
         />
       </td>
@@ -98,6 +93,7 @@ const ResizableRow = memo(function ({
         />
       ))}
     </tr>
-  )
-})
-export default ResizableRow
+  );
+});
+
+export default ResizableRow;
