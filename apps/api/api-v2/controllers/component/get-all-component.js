9387082af34a1e01d8a569e3032e8600d45f0c98
FedericoA15 <federicoalmeida15@gmail.com>
2023-07-26 16:29:02 -0300

    Ticekt #50437

--- apps/api/api-v2/controllers/component/get-all-component.js
+++ apps/api/api-v2/controllers/component/get-all-component.js
@@ -3,7 +3,18 @@ const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
 const getAllComponents = async (req, res, next) => {
-  const component = await models.ComponentModel.findAll()
+  const { pageId } = req.body
+
+  const page = await models.PageModel.findOne({ where: { id: pageId } })
+
+  if (!page) throw new ClientError('Error not found page', 400)
+
+  const component = await models.ComponentModel.findAll({
+    where: {
+      pageId
+    }
+  })
+
   if (!component) throw new ClientError('Error not found component', 400)
   response(res, 200, component)
 }
