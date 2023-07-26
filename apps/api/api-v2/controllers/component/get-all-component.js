a928d4ea2b822da60378c3e4f65faaa77ac253d0
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-07-26 20:26:23 -0300

    Update get-all-component.js

--- apps/api/api-v2/controllers/component/get-all-component.js
+++ apps/api/api-v2/controllers/component/get-all-component.js
@@ -3,15 +3,15 @@ const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
 const getAllComponents = async (req, res, next) => {
-  const { pageId } = req.body
+  const { id } = req.id
 
-  const page = await models.PageModel.findOne({ where: { id: pageId } })
+  const page = await models.PageModel.findOne({ where: { id } })
 
   if (!page) throw new ClientError('Error not found page', 400)
 
   const component = await models.ComponentModel.findAll({
     where: {
-      pageId
+      id
     }
   })
 
