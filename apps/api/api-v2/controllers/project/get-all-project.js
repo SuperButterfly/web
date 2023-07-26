d07f63941937235c2026bc0d49c3af0eb187d23b
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-26 10:54:07 -0300

    #50428 projects for user
    endpoint que traiga todos los projects por id de usuario

--- apps/api/api-v2/controllers/project/get-all-project.js
+++ apps/api/api-v2/controllers/project/get-all-project.js
@@ -1,9 +1,32 @@
 const { models } = require('../../database/connection/database')
 const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
+const { Op } = require('sequelize')
 
+/**
+ * Devuelve todos los proyectos de un usuario con filtro por el rol !== owner,
+ * esto se puede cambiar mandando el rol a excluir.
+ * @param {*} req userId, role: por defecto es Owner pero puede enviarse otro, que se envie sera excluido
+  */
 const getAllProject = async (req, res, next) => {
-  const allProjects = await models.ProjectModel.findAll()
+  const { userId, role } = req.body
+  const queryRole = role || 'Owner'
+  const allProjects = await models.ProjectModel.findAll({
+    include: [{
+      model: models.UserModel,
+      where: {
+        id: userId,
+      },
+      through: {
+        model: models.UserToolProjectModel,
+        where: {
+          role: {
+            [Op.not]: queryRole,
+          },
+        },
+      },
+    }],
+  })
 
   if (!allProjects) throw new ClientError('Error fetching the projects', 404)
 
