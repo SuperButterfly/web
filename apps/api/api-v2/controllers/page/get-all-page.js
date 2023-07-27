79060b6af6616141ae45555fed44f29495a77f86
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-27 14:56:32 -0300

    #50603 fix del get-all-page
    para q traiga las pages por projecto, hubo que arreglar la relacio

--- apps/api/api-v2/controllers/page/get-all-page.js
+++ apps/api/api-v2/controllers/page/get-all-page.js
@@ -3,17 +3,17 @@ const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
 
 const getAllPage = async (req, res, next) => {
-  const { projectId } = req.params
+  const { id } = req.params
 
-  const project = await models.ProjectModel.findByPK(projectId)
+  const project = await models.ProjectModel.findByPk(id)
 
   if (!project) throw new ClientError('Error project not found', 404)
 
   const allPages = await models.PageModel.findAll({
-    where: { projectId }
+    where: { projectId: id }
   })
-
-  if (!allPages) throw new ClientError('Error fetching the pages', 404)
+  console.log(allPages)
+  if (!allPages.length) throw new ClientError('Error fetching the pages', 404)
 
   response(res, 200, allPages)
 }
