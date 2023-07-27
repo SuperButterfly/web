aeed198b30fef702349a3b73fa864e67f158f671
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-07-27 14:57:33 -0300

    fix get project by rol

--- apps/api/api-v2/controllers/project/get-all-project.js
+++ apps/api/api-v2/controllers/project/get-all-project.js
@@ -1,37 +1,11 @@
 const { models } = require('../../database/connection/database')
 const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
-const { Op } = require('sequelize')
 
-/**
- * Devuelve todos los proyectos de un usuario con filtro por el rol !== owner,
- * esto se puede cambiar mandando el rol a excluir.
- * @param {*} req userId, role: por defecto es Owner pero puede enviarse otro, que se envie sera excluido
- */
 const getAllProject = async (req, res, next) => {
-  const { userId, role } = req.body
-  const queryRole = role || 'Owner'
-  const allProjects = await models.ProjectModel.findAll({
-    include: [
-      {
-        model: models.UserModel,
-        where: {
-          id: userId
-        },
-        through: {
-          model: models.UserToolProjectModel,
-          where: {
-            role: {
-              [Op.not]: queryRole
-            }
-          }
-        }
-      }
-    ]
-  })
-
-  if (!allProjects) throw new ClientError('Error fetching the projects', 404)
-
+  const allProjects = await models.ProjectModel.findAll()
+  if (!allProjects.length) throw new ClientError('Error fetching the projects', 404)
+  
   response(res, 200, allProjects)
 }
 
