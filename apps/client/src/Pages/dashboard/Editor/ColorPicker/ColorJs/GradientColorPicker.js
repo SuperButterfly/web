836fcb09e809cda8baf21efb9fa3f0f049562699
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2023-07-27 13:17:25 -0300

    gradient picker betterUp

--- apps/client/src/Pages/dashboard/Editor/ColorPicker/ColorJs/GradientColorPicker.js
+++ apps/client/src/Pages/dashboard/Editor/ColorPicker/ColorJs/GradientColorPicker.js
@@ -3,8 +3,19 @@ import ColorPicker, { useColorPicker } from 'react-best-gradient-color-picker'
 import styles from '../ColorStyleCss/Gradient.modules.css'
 
 const GradientColorPicker = () => {
-  const [color, setColor] = useState('rgba(255, 255, 255, 1)')
-  const { valueToHex, getGradientObject } = useColorPicker(color, setColor)
+  const [color, setColor] = useState(
+    'linear-gradient(90deg, rgba(96,93,93,1) 0%, rgba(255,255,255,1) 100%)'
+  )
+  const { valueToHSL, valueToHSV, valueToHex, valueToCmyk, rgbaArr, hslArr } =
+    useColorPicker(color, setColor)
+
+  /**convertidores nativos */
+  const hslString = valueToHSL()
+  const hsvString = valueToHSV()
+  const hexString = valueToHex()
+  const cmykString = valueToCmyk()
+  const rgbaArray = rgbaArr
+  const hslArray = hslArr
 
   const handleColorChange = (newColor) => {
     setColor(newColor)
@@ -15,12 +26,22 @@ const GradientColorPicker = () => {
     console.log(hexColor)
   }
 
-  const handleGetGradientObjectButtonClick = () => {
-    const gradientObject = getGradientObject()
-    console.log(gradientObject)
-  }
-
-  return <ColorPicker value={color} onChange={handleColorChange} />
+  // const handleGetGradientObjectButtonClick = () => {
+  //   const gradientObject = getGradientObject()
+  //   console.log(gradientObject)}
+  return (
+    <div>
+      <ColorPicker
+        hideEyeDrop={true}
+        hideAdvancedSliders={true}
+        hideColorGuide={true}
+        hideInputType={true}
+        hideColorTypeBtns={true}
+        value={color}
+        onChange={setColor}
+      />
+    </div>
+  )
 }
 
 export default GradientColorPicker
