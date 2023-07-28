8fb788e12356d774cc5f12707a017185a705a776
AgustinChavero <agustindanielchavero@gmail.com>
2023-07-28 12:25:26 -0300

    Fix add-color

--- apps/api/api-v2/controllers/color/add-color.js
+++ apps/api/api-v2/controllers/color/add-color.js
@@ -13,7 +13,37 @@ const addColor = async (req, res, next) => {
 
   if (!project) throw new ClientError('Error not found project', 400)
 
-  const newColor = await models.ColorModel.create({ name, value, presetId })
+  const defaultValue = [
+    {
+      name: 'Primary',
+      types: [
+        { value: '#003EB3' },
+        { value: '#0074F0' },
+        { value: '#14A9FF' },
+        { value: '#85DCFF' }
+      ]
+    },
+    {
+      name: 'Gray',
+      types: [{ value: '#595959' }, { value: '#999999' }, { value: '#D9D9D9' }]
+    },
+    {
+      name: 'Success',
+      types: [{ value: '#199033' }, { value: '#32A94C' }, { value: '#4CC366' }]
+    },
+    {
+      name: 'Danger',
+      types: [{ value: '#A22020' }, { value: '#BF2626' }, { value: '#E14747' }]
+    }
+  ]
+
+  const newValue = value.length === 0 ? defaultValue : value
+
+  const newColor = await models.ColorModel.create({
+    name,
+    value: newValue,
+    presetId
+  })
 
   if (!newColor) throw new ClientError('Error to create color', 400)
 
