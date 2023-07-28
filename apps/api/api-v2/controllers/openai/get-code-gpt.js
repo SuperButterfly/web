cfffa3494ef6bec3a339f55ec4adf5389a64c6f5
Jesse McCallums <101757754+Jessemccallums@users.noreply.github.com>
2023-07-28 10:27:52 -0400

    feat: agregando funcionalidades de gpt, primeros pasos

--- apps/api/api-v2/controllers/openai/get-code-gpt.js
+++ apps/api/api-v2/controllers/openai/get-code-gpt.js
@@ -1,19 +1,30 @@
 const { models } = require('../../database/connection/database')
 const { catchedAsync, response } = require('../../utils/err')
 const { ClientError } = require('../../utils/err/errors')
+const dotenv = require('dotenv'); // Importa el paquete dotenv
+const { Configuration, OpenAIApi } = require('openai');
+
+dotenv.config(); // Carga las variables de entorno desde .env
+const openaiApiKey = "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";
+
+// Configura la API key de OpenAI
+const configuration = new Configuration({
+  apiKey: openaiApiKey,
+});
+const openai = new OpenAIApi(configuration);
 
 const getGPT = async (req, res) =>  {
-  const { message  } = req.body
+  const data = req.query.message;
 
   const completion = await openai.createChatCompletion({
     model: "gpt-4",
     messages: [
-      { "role": "system", "content": "just show the code" },
-      { "role": "user", "content": message }, // Utiliza el mensaje de la consulta del usuario como contenido
+        { "role": "system", "content": "just show the code" },
+        { "role": "user", "content": data }, // Utiliza el mensaje de la consulta del usuario como contenido
     ],
-  });
+});
 
-  const answer = completion.data.choices[0].message.content;
+const answer = completion.data.choices[0].message.content;
 
 
   response(res, 200, answer)
