43b8355941bb165d4f8a5048439d891ceefa0713
DianaQuintana <dcqy.95@gmail.com>
2023-07-31 13:10:21 -0500

    0903-29-27/AddIconsInButtonNewTerminal

--- apps/client/src/Components/TabsBar/IconsDictionary/index.jsx
+++ apps/client/src/Components/TabsBar/IconsDictionary/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FaReact } from 'react-icons/fa';
 import { PiFileCss } from 'react-icons/pi';
+import { BsTerminal } from 'react-icons/bs';
 
 const IconsDictionary = ( type ) => {
   const iconsTab = {
     react : <FaReact size={15} color="#0f8fff" /> ,
     css : <PiFileCss size={15} color="#0f8fff" />,
+    terminal: <BsTerminal size={15} color="#0f8fff" />,
   }
 
   return iconsTab[type]
