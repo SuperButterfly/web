34c23d5986dbfb935715de03254f28f5c2f6e3b3
juan camilo <118995594+juancamilo1478@users.noreply.github.com>
2023-07-31 23:03:51 -0500

    feat:Documentation

--- apps/client/src/Pages/dashboard/help/Footer.jsx
+++ apps/client/src/Pages/dashboard/help/Footer.jsx
@@ -1,29 +1,28 @@
-import './Footer.css'
 
+import style from './Footer.module.css'
 function Footer() {
   return (
-    <div>
-      <footer className="contentinfo">
-        <div className="csh-footer-ask-footer">
-          <div className="csh-wrapper-footer">
-            <div className="csh-footer-ask-text-footer">
-              <p className="csh-footer-ask-text-title-footer">
+      <div className={style.contentinfo}>
+        <div className={style.cshFooterAskFooter}>
+          <div className={style.cshWrapperFooter}>
+            <div className={style.cshFooterAskTextFooter}>
+              <p className={style.cshFooterAskTextTitleFooter}>
                 Not finding what you are looking for?
               </p>
-              <p className="csh-footer-ask-text-label-footer">
+              <p className={style.cshFooterAskTextLabelFooter}>
                 Chat with us or send us an email.
               </p>
             </div>
-            <ul className="csh-footer-ask-buttons-footer">
+            <ul className={style.cshFooterAskButtonsFooter}>
               <li>
-                <span className="csh-button-footer">Chat with us</span>
+                <span className={style.cshButtonFooter}>Chat with us</span>
               </li>
               <li>
                 <span
                   aria-label="Send us an email"
                   href="mailto:hello@teleporthq.io"
                   role="button"
-                  className="csh-button-icon-email-footer"
+                  className={style.cshButtonIconEmailFooter}
                 >
                   Send us an email
                 </span>
@@ -31,15 +30,15 @@ function Footer() {
             </ul>
           </div>
         </div>
-        <div className="csh-footer-copyright-footer">
-          <div className="csh-wrapper-footer">
-            <span className="csh-footer-copyright-brand-footer">
-              <span className="csh-font-sans-regular-footer">© 2023</span>
+        <div className={style.cshFooterCopyrightFooter}>
+          <div className={style.cshWrapperFooter}>
+            <span className={style.cshFooterCopyrightBrandFooter}>
+              <span className={style.cshFontSansRegularFooter}>© 2023</span>
             </span>
           </div>
         </div>
-      </footer>
-    </div>
+        </div>
+
   )
 }
 
--- apps/client/src/Pages/dashboard/help/Header.jsx
+++ apps/client/src/Pages/dashboard/help/Header.jsx
@@ -1,46 +1,27 @@
-import './Header.css'
+import style from './Header.module.css'
 
 function Header() {
   return (
-    <div>
-      <header role="banner">
-        <div className="csh-wrapper">
-          <div className="csh-header-main">
-            <a href="/en/" role="none" className="csh-header-main-logo">
-              <img
-                src="https://storage.crisp.chat/users/helpdesk/website/60c4bca58bb99c00/teleportlogo-v24_1tjg8fo.svg"
-                alt="TeleportHQ Help Center"
-              />
-            </a>
-            <div role="none" className="csh-header-main-actions">
+      <header className={style.cshHeaderBackgroundBanner }>
+      <div className={style.divbagrount}>
+      <span className={style.bagrount}></span>
+        <div className={style.cshWrapper}>
+          <div className={style.cshHeaderMain}>     
+            <div role="none" className={style.cshHeaderMainActions}>
               <a
                 href="#"
                 target="_blank"
                 rel="noopener noreferrer"
                 role="none"
-                className="csh-header-main-actions-website"
+                className={style.cshHeaderMainActionsWebsite}
               >
-                <span className="csh-header-main-actions-website-itself">
+                <span className={style.cshHeaderMainActionsWebsiteItself}>
                   Go to website
                 </span>
               </a>
             </div>
-            <span className="csh-clear"></span>
           </div>
-          <div className="csh-header-title">Academy</div>
-          <form
-            action="/en/includes/search/"
-            role="search"
-            onSubmit="return false"
-            data-target-suggest="/en/includes/suggest/"
-            data-target-report="/en/includes/report/"
-            data-has-emphasis="true"
-            data-has-focus="false"
-            data-expanded="false"
-            data-pending="false"
-            className="csh-header-search"
-          >
-            <span className="csh-header-search-field">
+          <div className= {style.HeaderTitle}><h1>Academy</h1></div>       
               <input
                 type="search"
                 name="search_query"
@@ -56,25 +37,19 @@ function Header() {
                 onKeyDown="CrispHelpdeskCommon.key_search_field(event)"
                 onKeyUp="CrispHelpdeskCommon.type_search_field(this)"
                 onsearch="CrispHelpdeskCommon.search_search_field(this)"
-                className="csh-font-sans-regular"
+                className={style.cshFontSansRegular}
               />
-              <span className="csh-header-search-field-autocomplete"></span>
-              <span className="csh-header-search-field-ruler">
-                <span className="csh-header-search-field-ruler-text"></span>
-              </span>
-            </span>
-            <div className="csh-header-search-results"></div>
-          </form>
+            <div className={style.ccshHeaderSearchResults}></div>
         </div>
         <div
           data-tile="squares-in-squares"
           data-has-banner="true"
-          className="csh-header-background"
+          className={style.cshHeaderBackground}
         >
-          <span className="csh-header-background-banner"></span>
+        </div>
         </div>
       </header>
-    </div>
+ 
   )
 }
 
