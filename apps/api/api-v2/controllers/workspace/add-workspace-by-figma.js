469736d45c54704b8bc40658f69ee58ca2c117f3
mpermingeat <38232063+mpermingeat@users.noreply.github.com>
2023-08-01 08:36:47 -0300

    Update add-workspace-by-figma.js

--- apps/api/api-v2/controllers/workspace/add-workspace-by-figma.js
+++ apps/api/api-v2/controllers/workspace/add-workspace-by-figma.js
@@ -5,11 +5,11 @@ const fs = require('fs')
 const JSZip = require('jszip')
 const path = require('path')
 const cheerio = require('cheerio')
-
+const getOrderNumber = require('../../utils/helpers/getOrderNumber')
 
 const addWorkspaceByFigma = async (req, res, next) => {
   const file = req.file
-  console.log(file)
+  const { projectId } = req.query
 
   // Lee el contenido del archivo ZIP
   const data = await fs.promises.readFile(file.path)
@@ -25,6 +25,7 @@ const addWorkspaceByFigma = async (req, res, next) => {
 
   // Busca el archivo index.html dentro del ZIP
   const indexHtmlFile = zip.file('index.html')
+  const styleCssFile = zip.file('style.css') // Nuevo: Busca el archivo style.css dentro del ZIP
 
   if (!indexHtmlFile) {
     throw new ClientError('No se encontró el archivo index.html en el ZIP.')
@@ -32,6 +33,12 @@ const addWorkspaceByFigma = async (req, res, next) => {
 
   // Lee el contenido del archivo index.html
   const indexHtmlContent = await indexHtmlFile.async('string')
+
+  // Lee el contenido del archivo style.css
+  let styleCssContent = '' // Inicializamos como cadena vacía
+  if (styleCssFile) {
+    styleCssContent = await styleCssFile.async('string')
+  }
 
   // Parsea el contenido HTML usando cheerio
   const $ = cheerio.load(indexHtmlContent)
@@ -69,12 +76,96 @@ const addWorkspaceByFigma = async (req, res, next) => {
 
     return tagObject
   }
+
   // Obtiene el árbol de etiquetas HTML anidadas desde el elemento raíz
   const rootElement = $('html').get(0) // Assuming root is <html> tag
   const htmlTree = generateNestedTree(rootElement)
- 
-    response(res, 200, htmlTree)
+
+  // Crear la página
+  const page = await models.PageModel.create({
+    name: 'New Page', // Set the name of the page as desired
+    projectId,
+    title:
+      htmlTree.children.find(
+        (node) => node.tag === 'head'
+      )?.children.find((child) => child.tag === 'title')?.children[0]?.text || 'Untitled',
+  })
+
+  // Crear el componente 'body' asociado con la página
+  const bodyComponent = await models.ComponentModel.create({
+    tag: 'body',
+    order: 1,
+    attributes: {},
+    nativeAttributes: {},
+    isShow: true,
+    pageId: page.id,
+    projectId,
+  })
+
+  // Objeto para almacenar las clases CSS y sus estilos asociados
+  const cssStyles = {}
+
+  // Función para parsear el contenido de style.css y almacenar los estilos en cssStyles
+  const parseCssStyles = () => {
+    const styleBlocks = styleCssContent.split('}')
+
+    for (const block of styleBlocks) {
+      const selectorBlock = block.split('{')
+      if (selectorBlock.length === 2) {
+        const selector = selectorBlock[0].trim()
+        const styles = selectorBlock[1].trim().split(';')
+        const stylesObj = {}
+        for (const style of styles) {
+          const [property, value] = style.split(':')
+          if (property && value) {
+            stylesObj[property.trim()] = value.trim()
+          }
+        }
+        cssStyles[selector] = stylesObj
+      }
+    }
   }
-  
+
+  // Llamamos a la función para parsear el estilo CSS
+  parseCssStyles()
+
+  // Función para crear el componente y sus propiedades recursivamente
+  const createComponentsRecursively = async (node, parentId) => {
+    for (const childNode of node.children) {
+      const component = await models.ComponentModel.create({
+        tag: childNode.tag,
+        order: await getOrderNumber(parentId),
+        attributes: childNode.attributes,
+        nativeAttributes: {},
+        isShow: true,
+        pageId: page.id,
+        projectId,
+        parentId,
+      })
+
+      // Crear la propiedad "style" para el componente y asignar los estilos de la clase CSS
+      const componentClass = childNode.attributes.class // Asumimos que el atributo "class" contiene el nombre de la clase CSS
+
+      if (componentClass && cssStyles[componentClass]) {
+        await models.PropertyModel.create({
+          style: cssStyles[componentClass], // Asignar los estilos de la clase CSS al componente
+          isDeleted: false,
+          ComponentId: component.id, // Relacionar la propiedad con el componente creado
+        })
+      }
+
+      if (childNode.children) {
+        await createComponentsRecursively(childNode, component.id)
+      }
+    }
+  }
+
+  const bodyNode = htmlTree.children.find((node) => node.tag === 'body')
+  if (bodyNode) {
+    await createComponentsRecursively(bodyNode, bodyComponent.id)
+  }
+
+  response(res, 200, page)
+}
 
 module.exports = { addWorkspaceByFigma: catchedAsync(addWorkspaceByFigma) }
